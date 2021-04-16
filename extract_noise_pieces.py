7f604e2a1717b7eee68c2e43077bb21520134f53
yinjiuxun <jiuxun_yin@g.harvard.edu>
2021-04-15 21:54:27 -0400

    Update extract_noise_pieces.py
    1. Refactored the code with functions (easier to read)
    2. Added the detrend processing here for better thresholding denoising. Need to think more about this step. Not sure whether this can affect the later ML processing.

--- extract_noise_pieces.py
+++ extract_noise_pieces.py
@@ -5,7 +5,7 @@ Created on Tue Apr 13 10:37:54 2021
 
 @author: Yin9xun
 """
-#%%
+# %%
 import threshold_functions as threshold
 import os
 import numpy as np
@@ -25,116 +25,54 @@ from scipy import signal as sgn
 # import the modules with different threshold functions
 sys.path.append('./')
 
-#%% loop over catalog to read waveforms
-working_dir = './waveforms'
-#Read catalog first
-catalog = obspy.read_events(working_dir + '/events_by_distance.xml')
-print(catalog)
+# %% define the refactoring functions
 
-#%% make output directory
-figure_output_dir = working_dir + '/events_data_figures'
-if not os.path.exists(figure_output_dir):
-    os.makedirs(figure_output_dir)
+def STFT_thresholding_denoise(twin=100, toverlap=50, win_type='hann'):
+    # apply the thresholding method in the STFT to separate the noise and signals
+    f, t, Sxx = sgn.stft(data, fs, nperseg=int(twin/dt),
+                         noverlap=int(toverlap/dt), window=win_type)
+    # apply STFT and calculate the soft threshold value gammaN
+    I_positive = np.abs(Sxx) > 1e-16
+    # gammaN = np.sqrt(2*np.log(len(time_series))) * np.std(np.abs(Sxx[I_positive]).flatten()) #* 1.4826
+    gammaN = np.sqrt(
+        2*np.log(len(Sxx[I_positive]))) * np.std(np.abs(Sxx[I_positive]).flatten())
 
-processed_waveform_output_dir = working_dir + '/events_data_processed'
-if not os.path.exists(processed_waveform_output_dir):
-    os.makedirs(processed_waveform_output_dir)
+    # thresholded the TF domain
+    Sxx_thresholded = threshold.soft_threshold(Sxx, gammaN)
 
-noise_output_dir = working_dir + '/noise'
-if not os.path.exists(noise_output_dir):
-    os.makedirs(noise_output_dir)
-#%%
-i_event0 = np.random.randint(0, 193)
-plt.close('all')
-for i_event in [20]:  # range(len(catalog)):
-    event = catalog[i_event]
-    #% % extract the event information
-    event_time = event.origins[0].time
-    event_lon = event.origins[0].longitude
-    event_lat = event.origins[0].latitude
-    event_dep = event.origins[0].depth/1e3
-    event_mag = event.magnitudes[0].mag
+    time_temp, data_denoised = sgn.istft(Sxx_thresholded, fs, nperseg=int(twin/dt),
+                                         noverlap=int(toverlap/dt), window=win_type)
 
-    # read 3-component event wavefroms
-    event_name = "IU.XMAS" + ".M" + \
-        str(event_mag) + "." + event_time.strftime("%Y%m%d-%H%M%S")
-    fname = "/events_data/" + event_name + '.mseed'
+    # interpolate the denoised waveform to the same time axis as the original waveforms
+    data_denoised = np.interp(
+        time, time_temp, data_denoised, left=0, right=0)
 
-    try:
-        tr = obspy.read(working_dir + fname)
-    except:
-        print("Issue with " + "event " + event_time.strftime("%Y%m%d-%H%M%S"))
-        continue
+    return data_denoised
 
-    # initialize the dictionary to store waveforms
-    waveform_dict = {}
-    waveform_denoised_dict = {}
-    noise_dict = {}
 
-    # loop over each channel
-    plt.close('all')
-    for i_chan in range(len(tr)):
-        channel = tr[i_chan].stats.channel
-        st0 = tr[i_chan]
-        st = st0.copy()
-        #st.decimate(factor=5, strict_length=False)
-        data = st.data
-        time = st.times()
-        dt = st.stats.delta
-        fs = 1/dt
+def plot_waveforms():
+    # plot waveforms and save the figures
+    plt.figure(i_event, figsize=(12, 12))
+    plt.subplot(len(tr), 2, i_chan*2+1)
+    plt.plot(time, data, '-b', alpha=1)
 
-        # parameters about the STFT
-        twin = 100
-        toverlap = 50
-        win_type = 'hann'
+    plt.plot(time, data_denoised, '-r', linewidth=0.3, alpha=0.8, zorder=5)
+    if i_chan == 2:
+        plt.xlabel('Time (s)')
 
-        f, t, Sxx = sgn.stft(data, fs, nperseg=int(twin/dt),
-                             noverlap=int(toverlap/dt), window=win_type)
-        # apply STFT and calculate the soft threshold value gammaN
-        I_positive = np.abs(Sxx) > 1e-16
-        #gammaN = np.sqrt(2*np.log(len(time_series))) * np.std(np.abs(Sxx[I_positive]).flatten()) #* 1.4826
-        gammaN = np.sqrt(
-            2*np.log(len(Sxx[I_positive]))) * np.std(np.abs(Sxx[I_positive]).flatten())
+    plt.title(event_name + '.' + channel)
+    plt.subplot(len(tr), 2, i_chan*2+2)
+    plt.plot(time, data-data_denoised, '-k',
+             alpha=0.9, linewidth=0.3, zorder=3)
+    if i_chan == 2:
+        plt.xlabel('Time (s)')
 
-        # thresholded the TF domain
-        Sxx_thresholded = threshold.soft_threshold(Sxx, gammaN)
+    plt.ylim(-4e4, 4e4)
+    plt.title('Noise in ' + channel)
 
-        time_temp, data_denoised = sgn.istft(Sxx_thresholded, fs, nperseg=int(twin/dt),
-                                             noverlap=int(toverlap/dt), window=win_type)
 
-        # interpolate the denoised waveform to the same time axis as the original waveforms
-        data_denoised = np.interp(
-            time, time_temp, data_denoised, left=0, right=0)
-
-        # get the waveforms and noise at each channel
-        waveform_dict[channel] = data
-        waveform_denoised_dict[channel] = data_denoised
-        noise_dict[channel] = data - data_denoised
-
-        # plot the waveforms
-        plt.figure(i_event, figsize=(12, 12))
-        plt.subplot(len(tr), 2, i_chan*2+1)
-        plt.plot(time, data, '-b', alpha=1)
-
-        plt.plot(time, data_denoised, '-r', linewidth=0.3, alpha=0.8, zorder=5)
-        if i_chan == 2:
-            plt.xlabel('Time (s)')
-
-        plt.title(event_name + '.' + channel)
-        plt.subplot(len(tr), 2, i_chan*2+2)
-        plt.plot(time, data-data_denoised, '-k',
-                 alpha=0.9, linewidth=0.3, zorder=3)
-        if i_chan == 2:
-            plt.xlabel('Time (s)')
-
-        plt.ylim(-4e4, 4e4)
-        plt.title('Noise in ' + channel)
-
-        #plt.show()
-        plt.savefig(figure_output_dir + '/' + event_name + '.png')
-
-    # save the waveforms in ASDF format
-    # save the waveforms
+def save_event_waveforms(output_name):
+    # save the event information and denoised waveform to ASDF files
     tree = {
         'event_name': event_name,
         'event_mag': event_mag,
@@ -148,21 +86,128 @@ for i_event in [20]:  # range(len(catalog)):
     }
 
     waveform_ff = asdf.AsdfFile(tree)
-    waveform_ff.write_to(processed_waveform_output_dir +
-                         '/' + event_name + '.asdf')
-
-    # save the noise only
+    waveform_ff.write_to(output_name)
+    
+def save_noise(output_name):
+    # save the separated noise to ASDF files
     tree = {
         'noise': noise_dict,
         'noise_time': time
     }
     noise_ff = asdf.AsdfFile(tree)
-    noise_ff.write_to(noise_output_dir + '/' + event_name + '.asdf')
+    noise_ff.write_to(output_name)
 
 
-# #%% Try read the asdf data
-# ff = asdf.open('./waveforms/events_data_processed/IU.XMAS.M6.1.20210218-153047.asdf')
-# ff.tree
-# time_wave = ff.tree['waveform_time']
-# wave_1 = ff.tree['waveforms']['BH1']
-# plt.plot(time_wave, wave_1)
+# %% loop over catalog to read waveforms
+working_dir = './waveforms'
+# Read catalog first
+catalog = obspy.read_events(working_dir + '/events_by_distance.xml')
+print(catalog)
+
+# %% make output directory
+figure_output_dir = working_dir + '/events_data_figures'
+if not os.path.exists(figure_output_dir):
+    os.makedirs(figure_output_dir)
+
+processed_waveform_output_dir = working_dir + '/events_data_processed'
+if not os.path.exists(processed_waveform_output_dir):
+    os.makedirs(processed_waveform_output_dir)
+
+noise_output_dir = working_dir + '/noise'
+if not os.path.exists(noise_output_dir):
+    os.makedirs(noise_output_dir)
+# %%
+plt.close('all')
+for i_event in range(len(catalog)):
+    event = catalog[i_event]
+    # extract the event information
+    event_time = event.origins[0].time
+    event_lon = event.origins[0].longitude
+    event_lat = event.origins[0].latitude
+    event_dep = event.origins[0].depth/1e3
+    event_mag = event.magnitudes[0].mag
+
+    # read 3-component event wavefroms
+    event_name = "IU.XMAS" + ".M" + \
+        str(event_mag) + "." + event_time.strftime("%Y%m%d-%H%M%S")
+    fname = "/events_data/" + event_name + '.mseed'
+
+    try:
+        tr0 = obspy.read(working_dir + fname)
+    except:
+        print("Issue with " + "event " + event_time.strftime("%Y%m%d-%H%M%S"))
+        continue
+    
+    # detrend the signal (need to think more about this step.)
+    tr = tr0.copy()
+    dt = tr[0].stats.delta
+    tr.detrend("spline", order=3, dspline=int(600/dt))
+
+    # initialize the dictionary to store waveforms
+    waveform_dict = {}
+    waveform_denoised_dict = {}
+    noise_dict = {}
+
+    # loop over each channel
+    for i_chan in range(len(tr)):
+        channel = tr[i_chan].stats.channel
+        
+        # original data without any processing
+        data0 = tr0[i_chan].data
+        
+        # detrended data to be denoised
+        st = tr[i_chan]
+        data = st.data
+        
+        # other information about the waveforms
+        dt = st.stats.delta
+        fs = 1/dt
+        time = st.times()
+
+        # parameters about the STFT
+        twin = 100
+        toverlap = 50
+        win_type = 'hann'
+        
+        # STFT thresholding denoise
+        data_denoised = STFT_thresholding_denoise(twin=twin, 
+                                                  toverlap=toverlap,
+                                                  win_type=win_type)
+        # get the waveforms and noise at each channel
+        waveform_dict[channel] = data0
+        waveform_denoised_dict[channel] = data_denoised
+        noise_dict[channel] = data - data_denoised
+
+        # plot the waveforms
+        plot_waveforms()
+
+    # save the waveform figures
+    plt.savefig(figure_output_dir + '/' + event_name + '.png')
+    plt.close('all')
+
+    # save the waveforms in ASDF format
+    save_event_waveforms(processed_waveform_output_dir + '/' + event_name + '.asdf')
+
+    # save the noise only
+    save_noise(noise_output_dir + '/' + event_name + '.asdf')
+
+
+#%% Try read the asdf data
+ff = asdf.open('./waveforms/events_data_processed/IU.XMAS.M7.3.20190714-091050.asdf')
+ff.tree
+time_wave = ff.tree['waveform_time']
+wave_1 = ff.tree['waveforms']['BH1']
+wave_2 = ff.tree['waveforms_denoised']['BH1']
+plt.plot(time_wave, wave_1)
+plt.plot(time_wave, wave_2)
+plt.plot(time_wave, wave_1 - wave_2)
+plt.show()
+
+
+ff = asdf.open('./waveforms/noise/IU.XMAS.M7.3.20190714-091050.asdf')
+ff.tree
+time_wave = ff.tree['noise_time']
+wave_1 = ff.tree['noise']['BH1']
+plt.figure()
+plt.plot(time_wave, wave_1)
+plt.show()
