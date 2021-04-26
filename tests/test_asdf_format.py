8b62b972530d061bf4220d7200237c38da4463dd
yinjiuxun <jiuxun_yin@g.harvard.edu>
2021-04-26 13:07:17 -0400

    Update the test scripts
    Update test hdf5 and asdf
    These two scripts are simply used to test how to store data into different formats.

--- tests/test_asdf_format.py
+++ tests/test_asdf_format.py
@@ -45,3 +45,33 @@ af.tree['powers']['squares']
 import numpy as np
 expected = [x ** 2 for x in range(100)]
 np.equal(af.tree['powers']['squares'], expected).all()
+
+
+
+#%% functional form to store into ASDF format
+def save_event_waveforms_asdf(output_name):
+    # save the event information and denoised waveform to ASDF files
+    tree = {
+        'event_name': event_name,
+        'event_mag': event_mag,
+        'event_time': event_time.datetime,
+        'event_lon': event_lon,
+        'event_lat': event_lat,
+        'event_dep': event_dep,
+        'waveform_time': time,
+        'waveforms': waveform_dict,
+        'waveforms_denoised': waveform_denoised_dict
+    }
+
+    waveform_ff = asdf.AsdfFile(tree)
+    waveform_ff.write_to(output_name)
+
+
+def save_noise_asdf(output_name):
+    # save the separated noise to ASDF files
+    tree = {
+        'noise': noise_dict,
+        'noise_time': time
+    }
+    noise_ff = asdf.AsdfFile(tree)
+    noise_ff.write_to(output_name)
