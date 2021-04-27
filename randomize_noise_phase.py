7b17c08280ed7421d43b3d3035f82da3e5f1db17
yinjiuxun <jiuxun_yin@g.harvard.edu>
2021-04-26 22:50:54 -0400

    Update randomize_noise_phase.py
    Randomly produce Ricker wavelets and then produce the corresponding synthetic signals.

--- randomize_noise_phase.py
+++ randomize_noise_phase.py
@@ -19,6 +19,9 @@ import h5py
 # functions for STFT (spectrogram)
 from scipy import signal as sgn
 
+# import Ricker
+import ricker
+
 # %% Randomize the phase in the Fourier domain
 
 
@@ -67,20 +70,68 @@ def produce_randomized_noise(noise, num_of_random,
     noise_array = np.array(noise_array)
     return noise_array
 
-    # %% Try read the asdf data
-    # ff = asdf.open('./waveforms/events_data_processed/IU.XMAS.M7.3.20190714-091050.asdf')
-    # ff.tree
-    # time_wave = ff.tree['waveform_time']
-    # wave_1 = ff.tree['waveforms']['BH1']
-    # wave_2 = ff.tree['waveforms_denoised']['BH1']
+
+def plot_and_compare_randomized_noise():
+    plt.close('all')
+    plt.figure()
+    plt.plot(time_noise, noise_BH1, '-r')
+    plt.plot(time_noise, noise_BH1_random, '-b')
+
+    # % Compare the STFT distribution of noise and randomized noise, looks better.
+    # the randomization of noise in the Fourier domain seems to be able to wipe out
+    # the signal residual while keep the general STFT structure of noise
+
+    # TODO: refactor this part to generate randomized noise
+    twin = 100
+    toverlap = 50
+    win_type = 'hann'
+
+    # apply the thresholding method in the STFT to separate the noise and signals
+    f, t, Sxx = sgn.stft(noise_BH1, fs, nperseg=int(twin / dt),
+                         noverlap=int(toverlap / dt), window=win_type)
+    vmax = np.amax(abs(Sxx))
+
+    plt.figure()
+    plt.subplot(221)
+    plt.pcolormesh(t, f, np.abs(Sxx), shading='auto', vmax=vmax/1.2)
+    plt.title('STFT of origianl noise')
+    plt.ylim(0, 20)
+
+    plt.subplot(223)
+    plt.pcolormesh(t, f, np.abs(Sxx), shading='auto', vmax=vmax/1.2)
+    plt.title('STFT of origianl noise')
+    plt.ylim(0, 1)
+    plt.show()
+
+    # apply the thresholding method in the STFT to separate the noise and signals
+    f, t, Sxx = sgn.stft(noise_BH1_random, fs, nperseg=int(twin / dt),
+                         noverlap=int(toverlap / dt), window=win_type)
+
+    plt.subplot(222)
+    plt.pcolormesh(t, f, np.abs(Sxx), shading='auto', vmax=vmax/1.2)
+    plt.title('STFT of randomized noise')
+    plt.ylim(0, 20)
+    plt.show()
+
+    plt.subplot(224)
+    plt.pcolormesh(t, f, np.abs(Sxx), shading='auto', vmax=vmax/1.2)
+    plt.title('STFT of randomized noise')
+    plt.ylim(0, 1)
+    plt.show()
 
 
+# %%
+# initialize X_train and Y_train
+X_train = []
+Y_train = []
+
+# load the data
 hdf5_files = glob.glob('./waveforms/noise/*.hdf5')
-hdf5_files = [hdf5_files[np.random.randint(0, len(hdf5_files))]]
+#hdf5_files = [hdf5_files[np.random.randint(0, len(hdf5_files))]]
 
-# TODO: change to read HDF5 format
-for ifile, hdf5_file in enumerate(hdf5_files[0:1]):
-    
+N_random = 10  # randomized noise for each noise data
+for ifile, hdf5_file in enumerate(hdf5_files):#[0:1]):
+
     with h5py.File(hdf5_file, 'r') as f:
         time_noise = f['noise_time'][:]
         dt = time_noise[1]-time_noise[0]
@@ -90,92 +141,77 @@ for ifile, hdf5_file in enumerate(hdf5_files[0:1]):
         noise_BHZ = f['noise']['BHZ'][:]
         print(hdf5_file)
 
-rng = np.random.default_rng(seed=1)
-noise_BH1_random = randomization_noise(noise_BH1, rng=rng)
+    rng = np.random.default_rng(seed=1)
+    noise_BH1_random = randomization_noise(noise_BH1, rng=rng)
 
-plt.close('all')
-plt.figure()
-plt.plot(time_noise, noise_BH1, '-r')
-plt.plot(time_noise, noise_BH1_random, '-b')
+    # plot_and_compare_randomized_noise()
+    noise_BHZ_random = produce_randomized_noise(noise_BHZ, N_random, rng=rng)
 
-# % Compare the STFT distribution of noise and randomized noise, looks better.
-# the randomization of noise in the Fourier domain seems to be able to wipe out
-# the signal residual while keep the general STFT structure of noise
 
-# TODO: refactor this part to generate randomized noise
-twin = 100
-toverlap = 50
-win_type = 'hann'
+# % % #TODO: prepare the synthetic seismic signals
+# use ricker wavelet first? Only look at BHZ component first
+# the input synthetic waveforms can be other types
 
-# apply the thresholding method in the STFT to separate the noise and signals
-f, t, Sxx = sgn.stft(noise_BH1, fs, nperseg=int(twin / dt),
-                     noverlap=int(toverlap / dt), window=win_type)
-vmax = np.amax(abs(Sxx))
+    # duration and number of points in the Ricker wavelet
+    ricker_len = 60
+    npt_ricker = int(ricker_len/dt)
+    N_segments = int(noise_BHZ_random.shape[1]/npt_ricker)
+    syn_time = np.arange(0, npt_ricker) * dt
 
-plt.figure()
-plt.subplot(221)
-plt.pcolormesh(t, f, np.abs(Sxx), shading='auto', vmax=vmax/1.2)
-plt.title('STFT of origianl noise')
-plt.ylim(0, 20)
+    for i_seg in range(N_segments):
 
-plt.subplot(223)
-plt.pcolormesh(t, f, np.abs(Sxx), shading='auto', vmax=vmax/1.2)
-plt.title('STFT of origianl noise')
-plt.ylim(0, 1)
-plt.show()
+        # random parameters for the synthetics to set
+        peak_loc = np.random.random() * ricker_len
+        fc = np.random.random() * 5
+        amp = np.random.random() * 10
 
-# apply the thresholding method in the STFT to separate the noise and signals
-f, t, Sxx = sgn.stft(noise_BH1_random, fs, nperseg=int(twin / dt),
-                     noverlap=int(toverlap / dt), window=win_type)
+        syn_seismic = ricker.ricker(
+            f=fc, len=ricker_len, dt=dt, peak_loc=peak_loc)
+        syn_seismic = amp * np.std(noise_BHZ) * syn_seismic
+        
+        
 
-plt.subplot(222)
-plt.pcolormesh(t, f, np.abs(Sxx), shading='auto', vmax=vmax/1.2)
-plt.title('STFT of randomized noise')
-plt.ylim(0, 20)
-plt.show()
+        syn_signal = noise_BHZ[i_seg * npt_ricker:(i_seg + 1) * npt_ricker]  \
+            + syn_seismic
+        
+        X_train.append(syn_signal)
+        Y_train.append(syn_seismic)
+        
+X_train = np.array(X_train)
+Y_train = np.array(Y_train)
 
-plt.subplot(224)
-plt.pcolormesh(t, f, np.abs(Sxx), shading='auto', vmax=vmax/1.2)
-plt.title('STFT of randomized noise')
-plt.ylim(0, 1)
-plt.show()
+#%% save the prepared data
+with h5py.File('training_datasets.hdf5', 'w') as f:    
+    f.create_dataset('time', data=syn_time)
+    f.create_dataset('X_train', data=X_train)
+    f.create_dataset('Y_train', data=X_train)
 
-# %% Randomize the phase in the STFT domain
 
-# twin=100
-# toverlap=50
-# win_type='hann'
-
-# # apply the thresholding method in the STFT to separate the noise and signals
-# f, t, Sxx = sgn.stft(noise_BH1, fs, nperseg=int(twin / dt),
-#                          noverlap=int(toverlap / dt), window=win_type)
-# vmax = np.amax(abs(Sxx))
-
+#%%
 # plt.figure()
-# plt.pcolormesh(t, f, np.abs(Sxx), shading='auto', vmax=vmax/1.2)
-# plt.show()
-
-# # # check the distribution of STFT phase
-# # phase_Sxx = np.angle(Sxx.flatten())
-# # plt.figure()
-# # plt.hist(phase_Sxx)
-# # plt.show()
-# # # checked: normal distribution between -pi to pi
-
-# # produce the random phase shift
-# phase_angle_shift = (rd.rand(Sxx.shape[0], Sxx.shape[1])-0.5)*2*np.pi
-# phase_shift = np.exp(np.ones(Sxx.shape)*phase_angle_shift*1j)
-
-# Sxx_shifted = np.abs(Sxx) * phase_shift
+# plt.subplot(211)
+# plt.plot(syn_time, syn_seismic * amp * np.std(noise_BHZ))
+# plt.subplot(212)
+# plt.plot(syn_time, syn_signal)
 
 
-# time_temp, noise_BH1_random = sgn.istft(Sxx_shifted, fs, nperseg=int(twin / dt),
-#                                          noverlap=int(toverlap / dt), window=win_type)
+# %% #TODO: cut the noise pieces into several short segments
 
-# # interpolate the denoised waveform to the same time axis as the original waveforms
-# noise_BH1_random = np.interp(time_noise, time_temp, noise_BH1_random, left=0, right=0)
 
+# %% #TODO: randomly combine the synthetic signal with noise, reshape to the
+# proper dimension: nsample * nt * 1
+
+
+# Not in use now
+# #%% Some sort of dispersive signal
+# time = np.arange(0,4800)*dt+10
+# syn_seismic = np.sin(8*np.pi*10**(time/25)/20)/(time+2)**2
+
+# f, t, Sxx = sgn.spectrogram(syn_seismic, fs, nperseg=128, noverlap=64)
+
+# plt.close('all')
 # plt.figure()
-# plt.plot(time_noise, noise_BH1, '-r')
-# plt.plot(time_noise, noise_BH1_random, '-b')
-# plt.show()
+# plt.subplot(211)
+# plt.plot(time, syn_seismic)
+# plt.subplot(212)
+# plt.pcolormesh(t,f,Sxx, shading='auto')
