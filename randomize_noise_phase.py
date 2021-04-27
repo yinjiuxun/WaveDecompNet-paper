92f74c77c76d012d2a0dcc50a69618e8727ce3b6
yinjiuxun <jiuxun_yin@g.harvard.edu>
2021-04-27 11:05:55 -0400

    Fixed a bug.

--- randomize_noise_phase.py
+++ randomize_noise_phase.py
@@ -22,23 +22,24 @@ from scipy import signal as sgn
 # import Ricker
 import ricker
 
+
 # %% Randomize the phase in the Fourier domain
 
 
 def randomization_noise(noise, rng=np.random.default_rng(None)):
-    '''function to produce randomized noise by shiftint the phase
+    """function to produce randomized noise by shiftint the phase
     randomization_noise(noise, rng=np.random.default_rng(None))
     return randomized noise
-    '''
+    """
 
     s = scipy.fft.fft(noise)
-    phase_angle_shift = (rng.random(len(s))-0.5)*2*np.pi
+    phase_angle_shift = (rng.random(len(s)) - 0.5) * 2 * np.pi
     # make sure the inverse transform is real
     phase_angle_shift[0] = 0
-    phase_angle_shift[int(len(s)/2+1):(len(s)+1)] = -1 * \
-        np.flip(phase_angle_shift[1:int(len(s)/2+1)])
+    phase_angle_shift[int(len(s) / 2 + 1):(len(s) + 1)] = -1 * \
+                                                          np.flip(phase_angle_shift[1:int(len(s) / 2 + 1)])
 
-    phase_shift = np.exp(np.ones(s.shape)*phase_angle_shift*1j)
+    phase_shift = np.exp(np.ones(s.shape) * phase_angle_shift * 1j)
 
     # Here apply the phase shift in the entire domain
     # s_shifted = np.abs(s) * phase_shift
@@ -55,11 +56,11 @@ def randomization_noise(noise, rng=np.random.default_rng(None)):
 
 def produce_randomized_noise(noise, num_of_random,
                              rng=np.random.default_rng(None)):
-    '''function to produce num_of_random randomized noise
+    """function to produce num_of_random randomized noise
     produce_randomized_noise(noise, num_of_random,
                              rng=np.random.default_rng(None))
     return np array with shape of num_of_random x npt_noise
-    '''
+    """
 
     noise_array = []
     # randomly produce num_of_random pieces of random noise
@@ -93,12 +94,12 @@ def plot_and_compare_randomized_noise():
 
     plt.figure()
     plt.subplot(221)
-    plt.pcolormesh(t, f, np.abs(Sxx), shading='auto', vmax=vmax/1.2)
+    plt.pcolormesh(t, f, np.abs(Sxx), shading='auto', vmax=vmax / 1.2)
     plt.title('STFT of origianl noise')
     plt.ylim(0, 20)
 
     plt.subplot(223)
-    plt.pcolormesh(t, f, np.abs(Sxx), shading='auto', vmax=vmax/1.2)
+    plt.pcolormesh(t, f, np.abs(Sxx), shading='auto', vmax=vmax / 1.2)
     plt.title('STFT of origianl noise')
     plt.ylim(0, 1)
     plt.show()
@@ -108,13 +109,13 @@ def plot_and_compare_randomized_noise():
                          noverlap=int(toverlap / dt), window=win_type)
 
     plt.subplot(222)
-    plt.pcolormesh(t, f, np.abs(Sxx), shading='auto', vmax=vmax/1.2)
+    plt.pcolormesh(t, f, np.abs(Sxx), shading='auto', vmax=vmax / 1.2)
     plt.title('STFT of randomized noise')
     plt.ylim(0, 20)
     plt.show()
 
     plt.subplot(224)
-    plt.pcolormesh(t, f, np.abs(Sxx), shading='auto', vmax=vmax/1.2)
+    plt.pcolormesh(t, f, np.abs(Sxx), shading='auto', vmax=vmax / 1.2)
     plt.title('STFT of randomized noise')
     plt.ylim(0, 1)
     plt.show()
@@ -127,79 +128,73 @@ Y_train = []
 
 # load the data
 hdf5_files = glob.glob('./waveforms/noise/*.hdf5')
-#hdf5_files = [hdf5_files[np.random.randint(0, len(hdf5_files))]]
+# hdf5_files = [hdf5_files[np.random.randint(0, len(hdf5_files))]]
 
 N_random = 5  # randomized noise for each noise data
-for ifile, hdf5_file in enumerate(hdf5_files):#[0:1]):
+for ifile, hdf5_file in enumerate(hdf5_files):  # [0:1]):
 
     with h5py.File(hdf5_file, 'r') as f:
         time_noise = f['noise_time'][:]
-        dt = time_noise[1]-time_noise[0]
-        fs = 1/dt
+        dt = time_noise[1] - time_noise[0]
+        fs = 1 / dt
         noise_BH1 = f['noise']['BH1'][:]
         noise_BH2 = f['noise']['BH2'][:]
         noise_BHZ = f['noise']['BHZ'][:]
         print(hdf5_file)
 
     rng = np.random.default_rng(seed=1)
-    #noise_BH1_random = randomization_noise(noise_BH1, rng=rng)
+    # noise_BH1_random = randomization_noise(noise_BH1, rng=rng)
 
     # plot_and_compare_randomized_noise()
     noise_BHZ_random = produce_randomized_noise(noise_BHZ, N_random, rng=rng)
 
-
-# % % #TODO: prepare the synthetic seismic signals
-# use ricker wavelet first? Only look at BHZ component first
-# the input synthetic waveforms can be other types
+    # % % #TODO: prepare the synthetic seismic signals
+    # use ricker wavelet first? Only look at BHZ component first
+    # the input synthetic waveforms can be other types
 
     # duration and number of points in the Ricker wavelet
     ricker_len = 60
-    npt_ricker = int(ricker_len/dt)
-    N_segments = int(noise_BHZ_random.shape[1]/npt_ricker)
+    npt_ricker = int(ricker_len / dt)
+    N_segments = int(noise_BHZ_random.shape[1] / npt_ricker)
     syn_time = np.arange(0, npt_ricker) * dt
-    
+
     # loop over each piece of randomized noise
     for i_noise in range(N_random):
         noise_BHZ_now = noise_BHZ_random[i_noise, :]
         # loop over each segment
         for i_seg in range(N_segments):
-    
             # random parameters for the synthetics to set
             peak_loc = np.random.random() * ricker_len
             fc = np.random.random() * 5
             amp = np.random.random() * 10
-    
+
             syn_seismic = ricker.ricker(
                 f=fc, len=ricker_len, dt=dt, peak_loc=peak_loc)
             syn_seismic = amp * np.std(noise_BHZ_now) * syn_seismic
-            
-            
-    
-            syn_signal = noise_BHZ_now[i_seg * npt_ricker:(i_seg + 1) * npt_ricker]  \
-                + syn_seismic
-            
+
+            syn_signal = noise_BHZ_now[i_seg * npt_ricker:(i_seg + 1) * npt_ricker] \
+                         + syn_seismic
+
             X_train.append(syn_signal)
             Y_train.append(syn_seismic)
-        
+
 X_train = np.array(X_train)
 Y_train = np.array(Y_train)
 
-#%% save the prepared data
-with h5py.File('training_datasets.hdf5', 'w') as f:    
+# %% save the prepared data
+with h5py.File('training_datasets.hdf5', 'w') as f:
     f.create_dataset('time', data=syn_time)
     f.create_dataset('X_train', data=X_train)
-    f.create_dataset('Y_train', data=X_train)
+    f.create_dataset('Y_train', data=Y_train)
 
-
-#%%
-i = np.random.randint(0,X_train.shape[0])
+# %%
+i = np.random.randint(0, X_train.shape[0])
 plt.figure(1)
 plt.subplot(211)
-plt.plot(syn_time, Y_train[i,:])
+plt.plot(syn_time, Y_train[i, :])
 plt.subplot(212)
-plt.plot(syn_time, X_train[i,:])
+plt.plot(syn_time, X_train[i, :])
 plt.show()
-
 
 # %% #TODO: cut the noise pieces into several short segments
 
