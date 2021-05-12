5549fc24e188d5e0bac20aa446e4f84aa4c456ab
Jiuxun <jiuxun_yin@g.harvard.edu>
2021-05-12 10:56:41 -0400

    Update a new network structure with 38 features at the bottleneck

--- autoencoder_denoise_test.py
+++ autoencoder_denoise_test.py
@@ -16,7 +16,7 @@ from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.model_selection import train_test_split
 
 # %% Import the data
-with h5py.File('./training_datasets.hdf5', 'r') as f:
+with h5py.File('./training_datasets_pyrocko_Z.hdf5', 'r') as f:
     time = f['time'][:]
     X_train_original = f['X_train'][:]
     Y_train_original = f['Y_train'][:]
@@ -55,6 +55,10 @@ Y_train = Y_train.reshape(-1, Y_train.shape[1], 1)
 X_train, X_test, Y_train, Y_test = train_test_split(X_train, Y_train, test_size=0.4, random_state=13)
 X_validate, X_test, Y_validate, Y_test = train_test_split(X_test, Y_test, test_size=0.5, random_state=20)
 
+# %% Clean the original data
+del X_train_original
+del Y_train_original
+
 # %% build the architecture
 # %% Model the Data
 BATCH_SIZE = 128
@@ -70,7 +74,7 @@ import tensorflow as tf
 
 def autoencoder_test1():
     model = Sequential()
-    model.add(Conv1D(8, 7, padding='same', activation='relu', input_shape=(2400, 1)))
+    model.add(Conv1D(8, 7, padding='same', activation='relu', input_shape=(X_train.shape[1], 1)))
     model.add(MaxPooling1D(2))
     model.add(Conv1D(16, 7, padding='same', activation='relu'))
     model.add(MaxPooling1D(2))
@@ -102,36 +106,58 @@ def autoencoder_test1():
 def autoencoder_test_Conv1DTranspose():
     # use Conv1DTranspose instead
     model = Sequential()
-    model.add(Conv1D(8, 7, padding='same', activation='relu', input_shape=(2400, 1)))
-    model.add(MaxPooling1D(2))
+    model.add(Conv1D(8, 7, padding='same', activation='relu', input_shape=(X_train.shape[1], 1)))
+    model.add(MaxPooling1D(2)) #300
     model.add(Conv1D(16, 7, padding='same', activation='relu'))
-    model.add(MaxPooling1D(2))
+    model.add(MaxPooling1D(2)) #150
     model.add(Conv1D(16, 5, padding='same', activation='relu'))
-    model.add(MaxPooling1D(2))
-    model.add(Conv1D(32, 5, padding='same', activation='relu'))
-    model.add(MaxPooling1D(2))
+    model.add(MaxPooling1D(2)) #75
     model.add(Conv1D(32, 3, padding='same', activation='relu'))
-    model.add(MaxPooling1D(3))
+    model.add(MaxPooling1D(3)) #25
     model.add(Dropout(rate=0.1))
     model.add(Bidirectional(LSTM(units=16, return_sequences=True, dropout=0.1)))
     model.add(LSTM(units=16, return_sequences=True))
-    model.add(UpSampling1D(3))
+    model.add(UpSampling1D(3)) #75
     model.add(Conv1DTranspose(32, 3, padding='same', activation='relu'))
-    model.add(UpSampling1D(2))
-    model.add(Conv1DTranspose(32, 5, padding='same', activation='relu'))
-    model.add(UpSampling1D(2))
+    model.add(UpSampling1D(2)) #150
     model.add(Conv1DTranspose(16, 5, padding='same', activation='relu'))
-    model.add(UpSampling1D(2))
+    model.add(UpSampling1D(2)) #300
     model.add(Conv1DTranspose(16, 7, padding='same', activation='relu'))
-    model.add(UpSampling1D(2))
+    model.add(UpSampling1D(2)) #600
     model.add(Conv1DTranspose(8, 7, padding='same', activation='relu'))
     model.add(Conv1DTranspose(1, 7, padding='same'))
     model.add(LeakyReLU(alpha=0.5))
 
     return model
 
+def autoencoder_test_Conv1DTranspose2():
+    # use Conv1DTranspose instead
+    model = Sequential()
+    model.add(Conv1D(8, 7, padding='same', activation='relu', input_shape=(X_train.shape[1], 1)))
+    model.add(MaxPooling1D(2)) #300
+    model.add(Conv1D(16, 7, padding='same', activation='relu'))
+    model.add(MaxPooling1D(2)) #150
+    model.add(Conv1D(16, 5, padding='same', activation='relu'))
+    model.add(MaxPooling1D(2)) #75
+    model.add(Conv1D(32, 3, padding='same', activation='relu'))
+    model.add(MaxPooling1D(2, padding='same')) #37
+    model.add(Dropout(rate=0.1))
+    model.add(Bidirectional(LSTM(units=16, return_sequences=True, dropout=0.1)))
+    model.add(LSTM(units=16, return_sequences=True))
+    model.add(UpSampling1D(2)) #76
+    model.add(Conv1D(32, 2, activation='relu'))
+    model.add(UpSampling1D(2)) #152
+    model.add(Conv1DTranspose(16, 5, padding='same', activation='relu'))
+    model.add(UpSampling1D(2)) #300
+    model.add(Conv1DTranspose(16, 7, padding='same', activation='relu'))
+    model.add(UpSampling1D(2)) #600
+    model.add(Conv1DTranspose(8, 7, padding='same', activation='relu'))
+    model.add(Conv1DTranspose(1, 7, padding='same'))
+    model.add(LeakyReLU(alpha=0.5))
 
-model = autoencoder_test_Conv1DTranspose()
+    return model
+
+model = autoencoder_test_Conv1DTranspose2()
 #model = autoencoder_test1()
 
 print(model.summary())
@@ -158,18 +184,18 @@ plt.figure()
 plt.plot(loss, 'o', label='loss')
 plt.plot(val_loss, '-', label='Validation loss')
 plt.legend()
-plt.title('Ricker_Autoencoder_model_Conv1DTranspose')
+plt.title('Synthetic_seismogram_Z_Conv1DTranspose')
 plt.show()
-plt.savefig('./Figures/Loss_evolution.png')
+plt.savefig('./Figures/Loss_evolution_38.png')
 
 # %% Save the model
 model_dataset_dir = './Model_and_datasets'
 if not os.path.exists(model_dataset_dir):
     os.mkdir(model_dataset_dir)
-model.save(model_dataset_dir + '/Ricker_Autoencoder_model_Conv1DTranspose.hdf5')
+model.save(model_dataset_dir + '/Synthetic_seismogram_Z_Conv1DTranspose_38.hdf5')
 
 # %% Save the datasets
-model_datasets = model_dataset_dir + '/Ricker_Autoencoder_model_datasets.hdf5'
+model_datasets = model_dataset_dir + '/Synthetic_seismogram_Z_Autoencoder_model_datasets.hdf5'
 if not os.path.exists(model_datasets):
     with h5py.File(model_datasets, 'w') as f:
         f.create_dataset('time', data=time)
