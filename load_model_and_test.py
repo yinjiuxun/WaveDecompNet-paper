2a42d5cdcf74dec7b50d709f2ecb7b0abcf7e7f0
Jiuxun <jiuxun_yin@g.harvard.edu>
2021-05-12 13:42:32 -0400

    Now the network can handle 3-component data.

--- load_model_and_test.py
+++ load_model_and_test.py
@@ -5,9 +5,9 @@ import h5py
 import keras
 
 # %% load model
-model = keras.models.load_model('./Model_and_datasets/Synthetic_seismogram_Z_Conv1DTranspose_38.hdf5')
+model = keras.models.load_model('./Model_and_datasets/Synthetic_seismogram_Z_Conv1DTranspose_ENZ.hdf5')
 # %% load dataset
-with h5py.File('./Model_and_datasets/Synthetic_seismogram_Z_Autoencoder_model_datasets.hdf5', 'r') as f:
+with h5py.File('./Model_and_datasets/Synthetic_seismogram_Autoencoder_model_datasets_ENZ.hdf5', 'r') as f:
     time = f['time'][:]
     X_test = f['X_test'][:]
     Y_test = f['Y_test'][:]
@@ -15,23 +15,27 @@ with h5py.File('./Model_and_datasets/Synthetic_seismogram_Z_Autoencoder_model_da
 # %% predict the waveforms
 Y_predict = model.predict(X_test)
 
+components = "ENZ"
 # %% Visualize the results
-i_show = np.random.randint(0, X_test.shape[0], 10)
+i_show = np.random.randint(0, X_test.shape[0], 5)
 plt.close('all')
-fig, ax = plt.subplots(5, 2, figsize=(16, 12), squeeze=True, num=1, sharey=True, sharex=True)
-ax = ax.flatten()
-for i, axi in enumerate(ax):
-    axi.plot(time, X_test[i_show[i], :, 0], '-k', label='X test')
-    axi.plot(time, Y_test[i_show[i], :, 0], '-b', label='Y test')
-    axi.plot(time, Y_predict[i_show[i], :, 0], '-r', label='Y predicted')
-    if i == 0:
-        axi.legend()
-ax[-2].set_xlabel('Time (s)')
-ax[-1].set_xlabel('Time (s)')
+fig, ax = plt.subplots(5, 3, figsize=(16, 12), num=1, sharey=True, sharex=True)
+for i in range(5):
+    for j in range(3):
+        ax[i, j].plot(time, X_test[i_show[i], :, j], '-k', label='X test')
+        ax[i, j].plot(time, Y_test[i_show[i], :, j], '-b', label='Y test')
+        ax[i, j].plot(time, Y_predict[i_show[i], :, j], '-r', label='Y predicted')
+
+        if i == 0:
+            ax[i, j].set_title(components[j])
+            if j == 0:
+                ax[i, j].legend()
+        if i == 4:
+            ax[i, j].set_xlabel("Time (s)")
 plt.show()
-plt.savefig('./Figures/Synthetic_seismogram_Z_Model_prediction.png')
+plt.savefig('./Figures/Synthetic_seismogram_Model_prediction_ENZ.png')
 
 
 # %% Visualize the model
 from keras.utils.vis_utils import plot_model
-plot_model(model, to_file='./Figures/synthetic_seismograms_Z_model_plot.png', show_shapes=True, show_layer_names=True)
+plot_model(model, to_file='./Figures/synthetic_seismograms_model_plot_ENZ.png', show_shapes=True, show_layer_names=True)
