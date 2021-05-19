0a88454aa942927ef344914dce73f722f9e3017c
Working <jiuxun_yin@g.harvard.edu>
2021-05-19 16:40:35 -0400

    Update the load and test for easier use.

--- load_model_and_test.py
+++ load_model_and_test.py
@@ -3,16 +3,27 @@ from matplotlib import pyplot as plt
 import numpy as np
 import h5py
 import keras
+from sklearn.model_selection import train_test_split
+
+# %% Need to specify model_name first
+model_name = 'autoencoder_Conv1DTranspose_ENZ'
 
 # %% load model
-model = keras.models.load_model('./Model_and_datasets/Synthetic_seismogram_Z_Conv1DTranspose_ENZ.hdf5')
+model = keras.models.load_model('./Model_and_datasets/' + f'/{model_name}_Model.hdf5')
 # %% load dataset
-with h5py.File('./Model_and_datasets/Synthetic_seismogram_Autoencoder_model_datasets_ENZ.hdf5', 'r') as f:
+with h5py.File('./Model_and_datasets/processed_synthetic_datasets_ENZ.hdf5', 'r') as f:
     time = f['time'][:]
-    X_test = f['X_test'][:]
-    Y_test = f['Y_test'][:]
     X_train = f['X_train'][:]
     Y_train = f['Y_train'][:]
+
+# split the model based on the information provided by the model
+with h5py.File('./Model_and_datasets/' + f'/{model_name}_Dataset_split.hdf5', 'r') as f:
+    train_size = f.attrs['train_size']
+    rand_seed1 = f.attrs['rand_seed1']
+    rand_seed2 = f.attrs['rand_seed2']
+
+X_train, X_test, Y_train, Y_test = train_test_split(X_train, Y_train, train_size=0.6, random_state=rand_seed1)
+X_validate, X_test, Y_validate, Y_test = train_test_split(X_test, Y_test, test_size=0.5, random_state=rand_seed2)
 
 # %% predict the waveforms
 Y_predict = model.predict(X_test)
@@ -35,9 +46,9 @@ for i in range(5):
         if i == 4:
             ax[i, j].set_xlabel("Time (s)")
 plt.show()
-plt.savefig('./Figures/Synthetic_seismogram_Model_prediction_ENZ.png')
+plt.savefig(f'./Figures/{model_name}_Prediction.png')
 
 
 # %% Visualize the model
 from keras.utils.vis_utils import plot_model
-plot_model(model, to_file='./Figures/synthetic_seismograms_model_plot_ENZ.png', show_shapes=True, show_layer_names=True)
+plot_model(model, to_file=f'./Figures/{model_name}_Visual_model.png', show_shapes=True, show_layer_names=True)
