Corrected the softmax amplitude issue. The softmax requires summation of all 6 components to be one. So the amplutde of training Y should be scaled to 1 (times 1/3 here for 3 components).

autoencoder_2D_models.py
@@ -33,54 +33,169 @@ def autoencoder_Conv2D_Spectrogram2(input_shape):
     model = Sequential()
     model.add(Conv2D(16, 5, padding='same', input_shape=input_shape))
     model.add(LeakyReLU(alpha=0.5))
-    #model.add(BatchNormalization())
+    # model.add(BatchNormalization())
     model.add(MaxPooling2D((2, 2)))
     model.add(Conv2D(32, 3, padding='same'))
     model.add(LeakyReLU(alpha=0.5))
-    #model.add(BatchNormalization())
+    # model.add(BatchNormalization())
     model.add(MaxPooling2D((2, 2)))  # 150
     model.add(Conv2D(64, 3, padding='same'))
     model.add(LeakyReLU(alpha=0.5))
-    #model.add(BatchNormalization())
+    # model.add(BatchNormalization())
     model.add(Conv2D(64, 3, padding='same'))
     model.add(LeakyReLU(alpha=0.5))
-    #model.add(BatchNormalization())
+    # model.add(BatchNormalization())
     model.add(UpSampling2D((2, 2)))  # 150
     model.add(Conv2DTranspose(32, 3, padding='same'))
     model.add(LeakyReLU(alpha=0.5))
-    #model.add(BatchNormalization())
+    # model.add(BatchNormalization())
     model.add(UpSampling2D((2, 2)))  # 150
     model.add(Conv2DTranspose(16, 5, padding='same'))
     model.add(LeakyReLU(alpha=0.5))
-    #model.add(BatchNormalization())
+    # model.add(BatchNormalization())
     model.add(Conv2DTranspose(6, 1, padding='same'))
     model.add(LeakyReLU(alpha=0.5))
-    model.add(BatchNormalization()) #NEW
+    model.add(BatchNormalization())  # NEW
 
     return model, model_name
 
 
 def autoencoder_Conv2D_Spectrogram3(input_shape):
     """a model to deal with spectrogram data normalized to zero-mean and unit variance"""
+    model_name = "spectrogram_mask_l1_softmax"
+    model = Sequential()
+    model.add(Conv2D(16, 5, padding='same', activation='relu', input_shape=input_shape))
+    # model.add(BatchNormalization())
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(32, 3, padding='same', activation='relu'))
+    # model.add(BatchNormalization())
+    model.add(MaxPooling2D((2, 2)))  # 150
+    model.add(Conv2D(64, 3, padding='same', activation='relu'))
+    # model.add(BatchNormalization())
+    model.add(Conv2D(64, 3, padding='same', activation='relu'))
+    # model.add(BatchNormalization())
+    model.add(UpSampling2D((2, 2)))  # 150
+    model.add(Conv2DTranspose(32, 3, padding='same', activation='relu'))
+    # model.add(BatchNormalization())
+    model.add(UpSampling2D((2, 2)))  # 150
+    model.add(Conv2DTranspose(16, 5, padding='same', activation='relu'))
+    # model.add(BatchNormalization())
+    model.add(Conv2DTranspose(6, 1, padding='same',
+                              activity_regularizer=tf.keras.regularizers.l1(0.01),
+                              activation='softmax'))
+    model.compile(loss='mean_squared_logarithmic_error', optimizer='adam')
+
+    return model, model_name
+
+
+def autoencoder_Conv2D_Spectrogram4(input_shape):
+    """a model to deal with spectrogram data normalized to zero-mean and unit variance"""
     model_name = "spectrogram_mask_softmax"
     model = Sequential()
     model.add(Conv2D(16, 5, padding='same', activation='relu', input_shape=input_shape))
-    #model.add(BatchNormalization())
+    # model.add(BatchNormalization())
     model.add(MaxPooling2D((2, 2)))
     model.add(Conv2D(32, 3, padding='same', activation='relu'))
-    #model.add(BatchNormalization())
+    # model.add(BatchNormalization())
     model.add(MaxPooling2D((2, 2)))  # 150
     model.add(Conv2D(64, 3, padding='same', activation='relu'))
-    #model.add(BatchNormalization())
+    # model.add(BatchNormalization())
     model.add(Conv2D(64, 3, padding='same', activation='relu'))
-    #model.add(BatchNormalization())
+    # model.add(BatchNormalization())
     model.add(UpSampling2D((2, 2)))  # 150
     model.add(Conv2DTranspose(32, 3, padding='same', activation='relu'))
-    #model.add(BatchNormalization())
+    # model.add(BatchNormalization())
     model.add(UpSampling2D((2, 2)))  # 150
     model.add(Conv2DTranspose(16, 5, padding='same', activation='relu'))
-    #model.add(BatchNormalization())
+    # model.add(BatchNormalization())
     model.add(Conv2DTranspose(6, 1, padding='same', activation='softmax'))
+    # model.add(Conv2DTranspose(6, 1, padding='same',
+    #                           activity_regularizer=tf.keras.regularizers.l2(0.0),
+    #                           activation='softmax'))
+    model.compile(loss='mean_squared_logarithmic_error', optimizer='adam')
 
+    return model, model_name
+
+
+def autoencoder_Conv2D_Spectrogram76(input_shape):
+    """a model to deal with spectrogram data normalized to zero-mean and unit variance"""
+    model_name = "spectrogram_mask_l1_sigmoid"
+    model = Sequential()
+    model.add(Conv2D(16, 5, padding='same', activation='relu', input_shape=input_shape))
+    # model.add(BatchNormalization())
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(32, 3, padding='same', activation='relu'))
+    # model.add(BatchNormalization())
+    model.add(MaxPooling2D((2, 2)))  # 150
+    model.add(Conv2D(64, 3, padding='same', activation='relu'))
+    # model.add(BatchNormalization())
+    # model.add(Conv2D(64, 3, padding='same', activation='relu'))
+    # model.add(BatchNormalization())
+    model.add(UpSampling2D((2, 2)))  # 150
+    model.add(Conv2DTranspose(32, 3, padding='same', activation='relu'))
+    # model.add(BatchNormalization())
+    model.add(UpSampling2D((2, 2)))  # 150
+    model.add(Conv2DTranspose(16, 5, padding='same', activation='relu'))
+    # model.add(BatchNormalization())
+    # model.add(Conv2DTranspose(6, 1, padding='same', activation='softmax'))
+    model.add(Conv2DTranspose(6, 1, padding='same',
+                              activity_regularizer=tf.keras.regularizers.l1(0.01),
+                              activation='softmax'))
+    model.compile(loss='mean_squared_logarithmic_error', optimizer='adam')
+
+    return model, model_name
+
+
+def autoencoder_Conv2D_Spectrogram77(input_shape):
+    """a model to deal with spectrogram data normalized to zero-mean and unit variance"""
+    model_name = "spectrogram_mask_l1_sigmoid"
+    model = Sequential()
+    model.add(Conv2D(16, 5, padding='same', activation='relu', input_shape=input_shape))
+    # model.add(BatchNormalization())
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(32, 3, padding='same', activation='relu'))
+    # model.add(BatchNormalization())
+    model.add(MaxPooling2D((2, 2)))  # 150
+    model.add(Conv2D(64, 3, padding='same', activation='relu'))
+    # model.add(BatchNormalization())
+    # model.add(Conv2D(64, 3, padding='same', activation='relu'))
+    # model.add(BatchNormalization())
+    model.add(UpSampling2D((2, 2)))  # 150
+    model.add(Conv2DTranspose(32, 3, padding='same', activation='relu'))
+    # model.add(BatchNormalization())
+    model.add(UpSampling2D((2, 2)))  # 150
+    model.add(Conv2DTranspose(16, 5, padding='same', activation='relu'))
+    # model.add(BatchNormalization())
+    # model.add(Conv2DTranspose(6, 1, padding='same', activation='softmax'))
+    model.add(Conv2DTranspose(6, 1, padding='same',
+                              activity_regularizer=tf.keras.regularizers.l1(0.01),
+                              activation='softmax'))
+    model.compile(loss='mean_squared_logarithmic_error', optimizer='adam')
+
+    return model, model_name
+
+
+def autoencoder_Conv2D_Spectrogram78(input_shape):
+    """a model to deal with spectrogram data normalized to zero-mean and unit variance"""
+    model_name = "spectrogram_mask_softmax"
+    model = Sequential()
+    model.add(Conv2D(16, 5, padding='same', activation='relu', input_shape=input_shape))
+    # model.add(BatchNormalization())
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(32, 3, padding='same', activation='relu'))
+    # model.add(BatchNormalization())
+    model.add(MaxPooling2D((2, 2)))  # 150
+    model.add(Conv2D(64, 3, padding='same', activation='relu'))
+    # model.add(BatchNormalization())
+    model.add(Conv2D(64, 3, padding='same', activation='relu'))
+    # model.add(BatchNormalization())
+    model.add(UpSampling2D((2, 2)))  # 150
+    model.add(Conv2DTranspose(32, 3, padding='same', activation='relu'))
+    # model.add(BatchNormalization())
+    model.add(UpSampling2D((2, 2)))  # 150
+    model.add(Conv2DTranspose(16, 5, padding='same', activation='relu'))
+    # model.add(BatchNormalization())
+    model.add(Conv2DTranspose(6, 1, padding='same', activation='softmax'))
+    model.compile(loss='mean_squared_logarithmic_error', optimizer='adam')
 
     return model, model_name