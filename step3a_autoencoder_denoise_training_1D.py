16b47094cc66f7f149254a4e0f88c8d47de4205e
yinjiuxun <yinjiuxun@boslogin03.rc.fas.harvard.edu>
2021-07-29 20:24:59 -0400

    Update

--- step3a_autoencoder_denoise_training_1D.py
+++ step3a_autoencoder_denoise_training_1D.py
@@ -13,13 +13,13 @@ import h5py
 from sklearn.model_selection import train_test_split
 
 # make the output directory
-model_dataset_dir = './Model_and_datasets_1D'
+model_dataset_dir = './Model_and_datasets_1D_STEAD'
 if not os.path.exists(model_dataset_dir):
     os.mkdir(model_dataset_dir)
 
 
 # %% Read the pre-processed datasets
-model_datasets = './training_datasets/training_datasets_waveform.hdf5'
+model_datasets = './training_datasets/training_datasets_STEAD_waveform.hdf5'
 with h5py.File(model_datasets, 'r') as f:
     X_train = f['X_train'][:]
     Y_train = f['Y_train'][:]
