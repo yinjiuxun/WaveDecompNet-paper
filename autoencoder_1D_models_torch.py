8d5de5bb705031e938b0997ba6f94f34b72eb47a
yinjiuxun <jiuxun_yin@g.harvard.edu>
2021-09-07 13:07:10 -0400

    Add LSTM, attention, linear bottleneck to the network

--- autoencoder_1D_models_torch.py
+++ autoencoder_1D_models_torch.py
@@ -11,10 +11,14 @@
 import torch
 from torch import nn
 import torch.nn.functional as F
+import math
 
-class Autoencoder_Conv1D_deep(nn.Module):
-    def __init__(self, model_name): #TODO: FIGURE OUT THE PADDING AND STRIDE!
-        super(Autoencoder_Conv1D_deep, self).__init__()
+
+# The Encoder-Decoder model with variable bottleneck
+class Autoencoder_Conv1D(nn.Module):
+    def __init__(self, model_name, bottleneck=None):
+        """Class of the autoencoder, the type of the bottleneck can be specified"""
+        super(Autoencoder_Conv1D, self).__init__()
         self.model_name = model_name
         self.enc1 = nn.Conv1d(3, 8, 9, padding='same', dtype=torch.float64)
         self.enc2 = nn.Conv1d(8, 8, 9, stride=2, padding=4, dtype=torch.float64)
@@ -24,6 +28,8 @@ class Autoencoder_Conv1D_deep(nn.Module):
         self.enc6 = nn.Conv1d(32, 32, 5, stride=2, padding=2, dtype=torch.float64)
         self.enc7c = nn.Conv1d(32, 64, 3, padding='same', dtype=torch.float64)
         self.enc8 = nn.Conv1d(64, 64, 3, stride=3, dtype=torch.float64)
+
+        self.bottleneck = bottleneck
 
         self.dec1c = nn.ConvTranspose1d(64, 64, 3, stride=3, dtype=torch.float64)
         self.dec2 = nn.ConvTranspose1d(64, 32, 3, padding=1, dtype=torch.float64)
@@ -67,6 +73,20 @@ class Autoencoder_Conv1D_deep(nn.Module):
         x3 = self.enc7c(x)
         x = F.relu(self.bn7(self.enc8(x3)))
 
+        if self.bottleneck is not None:
+            # print(x.shape)
+            x = x.permute(0, 2, 1)  # change to (batch_size, num_steps, num_features)
+            # print(x.shape)
+
+            if isinstance(self.bottleneck, torch.nn.LSTM):
+                x, _ = self.bottleneck(x)  # LSTM will also output state variable
+            else:
+                x = self.bottleneck(x)
+
+            x = x.permute(0, 2, 1)  # change to (batch_size, num_features, num_steps)
+            # print(x.shape)
+
+
         x = self.dec1c(x)
         x = F.relu(self.bn8(x + x3))
         x = F.relu(self.bn9(self.dec2(x)))
@@ -82,82 +102,120 @@ class Autoencoder_Conv1D_deep(nn.Module):
         return x
 
 
-class Autoencoder_Conv1D_deep_LSTM(nn.Module):
-    """Add the 2-layer LSTM layer as the bottleneck"""
-    def __init__(self, model_name): #TODO: FIGURE OUT THE PADDING AND STRIDE!
-        super(Autoencoder_Conv1D_deep_LSTM, self).__init__()
-        self.model_name = model_name
-        self.enc1 = nn.Conv1d(3, 8, 9, padding='same', dtype=torch.float64)
-        self.enc2 = nn.Conv1d(8, 8, 9, stride=2, padding=4, dtype=torch.float64)
-        self.enc3c = nn.Conv1d(8, 16, 7, padding='same', dtype=torch.float64)
-        self.enc4 = nn.Conv1d(16, 16, 7, stride=2, padding=3, dtype=torch.float64)
-        self.enc5c = nn.Conv1d(16, 32, 5, padding='same', dtype=torch.float64)
-        self.enc6 = nn.Conv1d(32, 32, 5, stride=2, padding=2, dtype=torch.float64)
-        self.enc7c = nn.Conv1d(32, 64, 3, padding='same', dtype=torch.float64)
-        self.enc8 = nn.Conv1d(64, 64, 3, stride=3, dtype=torch.float64)
-        # # Consider the bottleneck here, size of enc8 output:[128, 64, 25]
-        self.bottleneck = nn.LSTM(64, 32, 2, bidirectional=True,
-                                  batch_first=True, dtype=torch.float64)
+# Below are the classes and functions used to define the self-attention
+class DotProductAttention(nn.Module):
+    """Scaled dot product attention."""
 
-        self.dec1c = nn.ConvTranspose1d(64, 64, 3, stride=3, dtype=torch.float64)
-        self.dec2 = nn.ConvTranspose1d(64, 32, 3, padding=1, dtype=torch.float64)
-        self.dec3c = nn.ConvTranspose1d(32, 32, 5, stride=2, padding=2, output_padding=1, dtype=torch.float64)
-        self.dec4 = nn.ConvTranspose1d(32, 16, 5, padding=2, dtype=torch.float64)
-        self.dec5c = nn.ConvTranspose1d(16, 16, 7, stride=2, padding=3, output_padding=1, dtype=torch.float64)
-        self.dec6 = nn.ConvTranspose1d(16, 8, 7, padding=3, dtype=torch.float64)
-        self.dec7 = nn.ConvTranspose1d(8, 8, 9, stride=2, padding=4, output_padding=1, dtype=torch.float64)
-        self.dec8 = nn.ConvTranspose1d(8, 3, 9, padding=4, dtype=torch.float64)
+    def __init__(self, dropout, **kwargs):
+        super(DotProductAttention, self).__init__(**kwargs)
+        self.dropout = nn.Dropout(dropout)
 
-        self.bn1 = nn.BatchNorm1d(8, dtype=torch.float64)
-        self.bn2 = nn.BatchNorm1d(8, dtype=torch.float64)
-        self.bn13 = nn.BatchNorm1d(8, dtype=torch.float64)
-        self.bn14 = nn.BatchNorm1d(8, dtype=torch.float64)
-
-        self.bn3 = nn.BatchNorm1d(16, dtype=torch.float64)
-        self.bn4 = nn.BatchNorm1d(16, dtype=torch.float64)
-        self.bn11 = nn.BatchNorm1d(16, dtype=torch.float64)
-        self.bn12 = nn.BatchNorm1d(16, dtype=torch.float64)
-
-        self.bn5 = nn.BatchNorm1d(32, dtype=torch.float64)
-        self.bn6 = nn.BatchNorm1d(32, dtype=torch.float64)
-        self.bn9 = nn.BatchNorm1d(32, dtype=torch.float64)
-        self.bn10 = nn.BatchNorm1d(32, dtype=torch.float64)
-
-        self.bn7 = nn.BatchNorm1d(64, dtype=torch.float64)
-        self.bn8 = nn.BatchNorm1d(64, dtype=torch.float64)
-
-        self.bn15 = nn.BatchNorm1d(3, dtype=torch.float64)
+    # Shape of `queries`: (`batch_size`, no. of queries, `d`)
+    # Shape of `keys`: (`batch_size`, no. of key-value pairs, `d`)
+    # Shape of `values`: (`batch_size`, no. of key-value pairs, value
+    # dimension)
+    # Shape of `valid_lens`: (`batch_size`,) or (`batch_size`, no. of queries)
+    def forward(self, queries, keys, values):
+        d = queries.shape[-1]
+        # Set `transpose_b=True` to swap the last two dimensions of `keys`
+        scores = torch.bmm(queries, keys.transpose(1, 2)) / math.sqrt(d)
+        self.attention_weights = F.softmax(scores)
+        return torch.bmm(self.dropout(self.attention_weights), values)
 
 
-    def forward(self, x):
-        x = F.relu(self.bn1(self.enc1(x)))
-        x = F.relu(self.bn2(self.enc2(x)))
-        x1 = self.enc3c(x)
-        x = F.relu(self.bn3(x1))
-        x = F.relu(self.bn4(self.enc4(x)))
-        x2 = self.enc5c(x)
-        x = F.relu(self.bn5(x2))
-        x = F.relu(self.bn6(self.enc6(x)))
-        x3 = self.enc7c(x)
-        x = F.relu(self.bn7(self.enc8(x3)))
+class PositionalEncoding(nn.Module):
+    """Positional encoding."""
 
-        #print(x.shape)
-        x = x.permute(0, 2, 1)
-        #print(x.shape)
-        x, _ = self.bottleneck(x)
-        x = x.permute(0, 2, 1)
-        #print(x.shape)
+    def __init__(self, num_hiddens, dropout, max_len=1000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(dropout)
+        # Create a long enough `P`
+        self.P = torch.zeros((1, max_len, num_hiddens))
+        X = torch.arange(max_len, dtype=torch.float32).reshape(
+            -1, 1) / torch.pow(
+            10000,
+            torch.arange(0, num_hiddens, 2, dtype=torch.float32) /
+            num_hiddens)
+        self.P[:, :, 0::2] = torch.sin(X)
+        self.P[:, :, 1::2] = torch.cos(X)
 
-        x = self.dec1c(x)
-        x = F.relu(self.bn8(x + x3))
-        x = F.relu(self.bn9(self.dec2(x)))
-        x = self.dec3c(x)
-        x = F.relu(self.bn10(x + x2))
-        x = F.relu(self.bn11(self.dec4(x)))
-        x = self.dec5c(x)
-        x = F.relu(self.bn12(x + x1))
-        x = F.relu(self.bn13(self.dec6(x)))
-        x = F.relu(self.bn14(self.dec7(x)))
-        x = self.bn15(self.dec8(x))
+    def forward(self, X):
+        X = X + self.P[:, :X.shape[1], :].to(X.device)
+        return self.dropout(X)
 
-        return x
+
+def transpose_qkv(X, num_heads):
+    """Transposition for parallel computation of multiple attention heads."""
+    # Shape of input `X`:
+    # (`batch_size`, no. of queries or key-value pairs, `num_hiddens`).
+    # Shape of output `X`:
+    # (`batch_size`, no. of queries or key-value pairs, `num_heads`,
+    # `num_hiddens` / `num_heads`)
+    X = X.reshape(X.shape[0], X.shape[1], num_heads, -1)
+
+    # Shape of output `X`:
+    # (`batch_size`, `num_heads`, no. of queries or key-value pairs,
+    # `num_hiddens` / `num_heads`)
+    X = X.permute(0, 2, 1, 3)
+
+    # Shape of `output`:
+    # (`batch_size` * `num_heads`, no. of queries or key-value pairs,
+    # `num_hiddens` / `num_heads`)
+    return X.reshape(-1, X.shape[2], X.shape[3])
+
+
+def transpose_output(X, num_heads):
+    """Reverse the operation of `transpose_qkv`."""
+    X = X.reshape(-1, num_heads, X.shape[1], X.shape[2])
+    X = X.permute(0, 2, 1, 3)
+    return X.reshape(X.shape[0], X.shape[1], -1)
+
+
+class MultiHeadAttention(nn.Module):
+    """Multi-head attention."""
+
+    def __init__(self, key_size, query_size, value_size, num_hiddens,
+                 num_heads, dropout, bias=False, **kwargs):
+        super(MultiHeadAttention, self).__init__(**kwargs)
+        self.num_heads = num_heads
+        self.attention = DotProductAttention(dropout)
+        self.W_q = nn.Linear(query_size, num_hiddens, bias=bias, dtype=torch.float64)
+        self.W_k = nn.Linear(key_size, num_hiddens, bias=bias, dtype=torch.float64)
+        self.W_v = nn.Linear(value_size, num_hiddens, bias=bias, dtype=torch.float64)
+        self.W_o = nn.Linear(num_hiddens, num_hiddens, bias=bias, dtype=torch.float64)
+
+    def forward(self, queries, keys, values):
+        # Shape of `queries`, `keys`, or `values`:
+        # (`batch_size`, no. of queries or key-value pairs, `num_hiddens`)
+        # Shape of `valid_lens`:
+        # (`batch_size`,) or (`batch_size`, no. of queries)
+        # After transposing, shape of output `queries`, `keys`, or `values`:
+        # (`batch_size` * `num_heads`, no. of queries or key-value pairs,
+        # `num_hiddens` / `num_heads`)
+        queries = transpose_qkv(self.W_q(queries), self.num_heads)
+        keys = transpose_qkv(self.W_k(keys), self.num_heads)
+        values = transpose_qkv(self.W_v(values), self.num_heads)
+
+        # Shape of `output`: (`batch_size` * `num_heads`, no. of queries,
+        # `num_hiddens` / `num_heads`)
+        output = self.attention(queries, keys, values)
+
+        # Shape of `output_concat`:
+        # (`batch_size`, no. of queries, `num_hiddens`)
+        output_concat = transpose_output(output, self.num_heads)
+        return self.W_o(output_concat)
+
+
+class Attention_bottleneck(nn.Module):
+    def __init__(self, num_hiddens, num_heads, dropout, bias=False, **kwargs):
+        super(Attention_bottleneck, self).__init__(**kwargs)
+        self.pe = PositionalEncoding(num_hiddens, dropout)
+        self.attention = MultiHeadAttention(num_hiddens, num_hiddens,
+                                            num_hiddens, num_hiddens,
+                                            num_heads, dropout)
+
+    def forward(self, X):
+        # shape of X: (batch_size, num_steps, num_features)
+        X = self.pe(X)  # postional encoding
+        X = self.attention(X, X, X)  # self attention
+        return X
--- step3a_autoencoder_denoise_training_1D.py
+++ step3a_autoencoder_denoise_training_1D.py
@@ -18,7 +18,7 @@ from utilities import mkdir
 import torch
 from torch_tools import WaveformDataset, EarlyStopping, try_gpu, training_loop
 from torch.utils.data import DataLoader
-from autoencoder_1D_models_torch import Autoencoder_Conv1D_deep
+from autoencoder_1D_models_torch import Autoencoder_Conv1D, Attention_bottleneck
 
 # make the output directory
 model_dataset_dir = './Model_and_datasets_1D_STEAD'
@@ -31,38 +31,55 @@ with h5py.File(model_datasets, 'r') as f:
     Y_train = f['Y_train'][:]
 
 # 3. split to training (60%), validation (20%) and test (20%)
-train_size = 0.1 #0.6
-test_size = 0.9 #0.5
+train_size = 0.6
+test_size = 0.5
 rand_seed1 = 13
 rand_seed2 = 20
-X_train, X_test, Y_train, Y_test = train_test_split(X_train, Y_train, train_size=train_size, random_state=rand_seed1)
-X_validate, X_test, Y_validate, Y_test = train_test_split(X_test, Y_test, test_size=test_size, random_state=rand_seed2)
+X_train, X_test, Y_train, Y_test = train_test_split(X_train, Y_train,
+                                                    train_size=train_size, random_state=rand_seed1)
+X_validate, X_test, Y_validate, Y_test = train_test_split(X_test, Y_test,
+                                                          test_size=test_size, random_state=rand_seed2)
 
 # Convert to the dataset class for Pytorch (here simply load all the data,
 # but for the sake of memory, can also use WaveformDataset_h5)
 training_data = WaveformDataset(X_train, Y_train)
 validate_data = WaveformDataset(X_validate, Y_validate)
 
-model_name = "Autoencoder_conv1d_pytorch"
+# # The encoder-decoder model with self-attention bottleneck
+# model_name = "Autoencoder_Conv1D_attention"
+# bottleneck = Attention_bottleneck(64, 4, 0.2)  # Add the attention bottleneck
+#
+# # The encoder-decoder model with LSTM bottleneck
+# model_name = "Autoencoder_Cov1D_LSTM"
+# bottleneck = torch.nn.LSTM(64, 32, 2, bidirectional=True,
+#                            batch_first=True, dtype=torch.float64)
+
+# Linear bottleneck
+# model_name = "Autoencoder_Conv1D_Linear"
+# bottleneck = torch.nn.Linear(64, 64, dtype=torch.float64)
+
+# Model without specified bottleneck
+model_name = "Autoencoder_Conv1D_None"
+bottleneck = None
+
+model = Autoencoder_Conv1D(model_name, bottleneck).to(device=try_gpu())
+
+# make the output directory to store the model information
+model_dataset_dir = model_dataset_dir + '/' + model_name
+mkdir(model_dataset_dir)
+
 batch_size, epochs, lr = 128, 3, 1e-3
-model = Autoencoder_Conv1D_deep(model_name).to(device=try_gpu())
-# X = torch.randn(128, 3, 600, dtype=torch.float64).to(device=try_gpu())
-# Y = model(X)
+patience = 10  # patience of the early stopping
 
 loss_fn = torch.nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-
 train_iter = DataLoader(training_data, batch_size=batch_size, shuffle=True)
 validate_iter = DataLoader(validate_data, batch_size=batch_size, shuffle=True)
 
 model, avg_train_losses, avg_valid_losses = training_loop(train_iter, validate_iter,
                                                           model, loss_fn, optimizer,
-                                                          epochs=epochs, patience=20, device=try_gpu())
+                                                          epochs=epochs, patience=patience, device=try_gpu())
 print("Training is done!")
-
-# make the output directory to store the model information
-model_dataset_dir = model_dataset_dir + '/' + model_name
-mkdir(model_dataset_dir)
 
 # %% Save the model
 torch.save(model, model_dataset_dir + f'/{model_name}_Model.pth')
