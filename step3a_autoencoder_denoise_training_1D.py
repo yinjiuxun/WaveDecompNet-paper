777f564e636089366e0bc0b477b0c9a255a10e88
yinjiuxun <yinjiuxun@holylogin04.rc.fas.harvard.edu>
2021-09-07 17:48:59 -0400

    change parameters

--- step3a_autoencoder_denoise_training_1D.py
+++ step3a_autoencoder_denoise_training_1D.py
@@ -46,14 +46,14 @@ X_validate, X_test, Y_validate, Y_test = train_test_split(X_test, Y_test,
 training_data = WaveformDataset(X_train, Y_train)
 validate_data = WaveformDataset(X_validate, Y_validate)
 
-# The encoder-decoder model with self-attention bottleneck
-model_name = "Autoencoder_Conv1D_attention"
-bottleneck = Attention_bottleneck(64, 4, 0.2)  # Add the attention bottleneck
+# # The encoder-decoder model with self-attention bottleneck
+# model_name = "Autoencoder_Conv1D_attention"
+# bottleneck = Attention_bottleneck(64, 4, 0.2)  # Add the attention bottleneck
 #
-# # The encoder-decoder model with LSTM bottleneck
-# model_name = "Autoencoder_Cov1D_LSTM"
-# bottleneck = torch.nn.LSTM(64, 32, 2, bidirectional=True,
-#                            batch_first=True, dtype=torch.float64)
+# The encoder-decoder model with LSTM bottleneck
+model_name = "Autoencoder_Cov1D_LSTM"
+bottleneck = torch.nn.LSTM(64, 32, 2, bidirectional=True,
+                           batch_first=True, dtype=torch.float64)
 
 # # Linear bottleneck
 # model_name = "Autoencoder_Conv1D_Linear"
