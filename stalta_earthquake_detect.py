fa4c6ccbfb233d33871c9869cdf6251c38d17c1e
yinjiuxun <jiuxun_yin@g.harvard.edu>
2021-11-13 13:20:33 -0500

    Apply recursive STA/LTA on the separated earthquake waveform to see the improvement on the data.

--- stalta_earthquake_detect.py
+++ stalta_earthquake_detect.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot as plt
 import h5py
 
 import obspy
-
+from utilities import mkdir
 import matplotlib
 
 matplotlib.rcParams.update({'font.size': 12})
@@ -16,9 +16,10 @@ working_dir = os.getcwd()
 # waveforms
 waveform_dir = working_dir + '/continuous_waveforms'
 
+network_station = 'IU.POHA'
 second_per_day = 24 * 3600
-tr_earthquake = obspy.read(waveform_dir + '/' + 'IU.POHA.00.20210731-20210901_separated_earthquake.mseed')
-tr_raw = obspy.read(waveform_dir + '/' + 'IU.POHA.00.20210731-20210901_original_earthquake.mseed')
+tr_earthquake = obspy.read(waveform_dir + '/' + network_station + '.00.20210731-20210901_separated_earthquake.mseed')
+tr_raw = obspy.read(waveform_dir + '/' + network_station + '.00.20210731-20210901_original_earthquake.mseed')
 
 scaling_factor_waveform = 1e5
 for i in range(3):
@@ -63,105 +64,153 @@ detect_duration1 = np.array([trig1[i]['duration'] for i in range(len(trig1))])
 detect_time2 = np.array([trig2[i]['time'] - st2[0].stats.starttime for i in range(len(trig2))])
 detect_duration2 = np.array([trig2[i]['duration'] for i in range(len(trig2))])
 
-plt.close('all')
 # plot original data and STA/LTA results
-fig, ax = plt.subplots(4, 2, sharex=True, figsize=(14, 12))
+def plot_long_waveform_for_visualization():
+    fig, ax = plt.subplots(4, 2, sharex=True, figsize=(14, 12))
 
-# Original data
-ax[0, 0].plot(waveform_time / second_per_day, cft1_E, color=ENZ_color[0])
-ax[0, 0].plot(waveform_time / second_per_day, cft1_N, color=ENZ_color[1])
-ax[0, 0].plot(waveform_time / second_per_day, cft1_Z, color=ENZ_color[2])
+    # Original data
+    ax[0, 0].plot(waveform_time / second_per_day, cft1_E, color=ENZ_color[0])
+    ax[0, 0].plot(waveform_time / second_per_day, cft1_N, color=ENZ_color[1])
+    ax[0, 0].plot(waveform_time / second_per_day, cft1_Z, color=ENZ_color[2])
 
-ax[0, 0].plot(waveform_time[[0, -1]] / second_per_day, np.ones((2, 1)) * trigger_on, color='k')
-ax[0, 0].plot(waveform_time[[0, -1]] / second_per_day, np.ones((2, 1)) * trigger_off, '--k')
-ax[0, 0].set_title('Original waveform')
-ax[0, 0].set_ylabel('STA/LTA ratio')
+    ax[0, 0].plot(waveform_time[[0, -1]] / second_per_day, np.ones((2, 1)) * trigger_on, color='k')
+    ax[0, 0].plot(waveform_time[[0, -1]] / second_per_day, np.ones((2, 1)) * trigger_off, '--k')
+    ax[0, 0].set_title('Original waveform')
+    ax[0, 0].set_ylabel('STA/LTA ratio')
 
-ax[1, 0].plot(waveform_time / second_per_day, tr_raw[0].data, color=ENZ_color[0])
-ax[1, 0].plot(detect_time1 / second_per_day, np.ones(detect_time1.shape) * np.mean(tr_raw[0].data), 'kx')
-axi = ax[1, 0]
-ax[1, 0].set_ylabel('E component')
+    ax[1, 0].plot(waveform_time / second_per_day, tr_raw[0].data, color=ENZ_color[0])
+    ax[1, 0].plot(detect_time1 / second_per_day, np.ones(detect_time1.shape) * np.mean(tr_raw[0].data), 'kx')
+    axi = ax[1, 0]
+    ax[1, 0].set_ylabel('E component')
 
-ax[2, 0].plot(waveform_time / second_per_day, tr_raw[1].data, color=ENZ_color[1])
-ax[2, 0].plot(detect_time1 / second_per_day, np.ones(detect_time1.shape) * np.mean(tr_raw[1].data), 'kx')
-ax[2, 0].sharey(axi)
-ax[2, 0].set_ylabel('N component')
+    ax[2, 0].plot(waveform_time / second_per_day, tr_raw[1].data, color=ENZ_color[1])
+    ax[2, 0].plot(detect_time1 / second_per_day, np.ones(detect_time1.shape) * np.mean(tr_raw[1].data), 'kx')
+    ax[2, 0].sharey(axi)
+    ax[2, 0].set_ylabel('N component')
 
-ax[3, 0].plot(waveform_time / second_per_day, tr_raw[2].data, color=ENZ_color[2])
-ax[3, 0].plot(detect_time1 / second_per_day, np.ones(detect_time1.shape) * np.mean(tr_raw[2].data), 'kx')
-ax[3, 0].sharey(axi)
-ax[3, 0].set_ylabel('Z component')
+    ax[3, 0].plot(waveform_time / second_per_day, tr_raw[2].data, color=ENZ_color[2])
+    ax[3, 0].plot(detect_time1 / second_per_day, np.ones(detect_time1.shape) * np.mean(tr_raw[2].data), 'kx')
+    ax[3, 0].sharey(axi)
+    ax[3, 0].set_ylabel('Z component')
 
-# Separated data
-ax[0, 1].plot(waveform_time / second_per_day, cft2_E, color=ENZ_color[0])
-ax[0, 1].plot(waveform_time / second_per_day, cft2_N, color=ENZ_color[1])
-ax[0, 1].plot(waveform_time / second_per_day, cft2_Z, color=ENZ_color[2])
+    # Separated data
+    ax[0, 1].plot(waveform_time / second_per_day, cft2_E, color=ENZ_color[0])
+    ax[0, 1].plot(waveform_time / second_per_day, cft2_N, color=ENZ_color[1])
+    ax[0, 1].plot(waveform_time / second_per_day, cft2_Z, color=ENZ_color[2])
 
-ax[0, 1].plot(waveform_time[[0, -1]] / second_per_day, np.ones((2, 1)) * trigger_on, color='k')
-ax[0, 1].plot(waveform_time[[0, -1]] / second_per_day, np.ones((2, 1)) * trigger_off, '--k')
-ax[0, 1].sharey(ax[0, 0])
-ax[0, 1].set_ylim(-0.5, 15)
-ax[0, 1].set_title('Separated waveform')
+    ax[0, 1].plot(waveform_time[[0, -1]] / second_per_day, np.ones((2, 1)) * trigger_on, color='k')
+    ax[0, 1].plot(waveform_time[[0, -1]] / second_per_day, np.ones((2, 1)) * trigger_off, '--k')
+    ax[0, 1].sharey(ax[0, 0])
+    ax[0, 1].set_ylim(-0.5, 15)
+    ax[0, 1].set_title('Separated waveform')
 
-ax[1, 1].plot(waveform_time / second_per_day, tr_earthquake[0].data, color=ENZ_color[0])
-ax[1, 1].plot(detect_time2 / second_per_day, np.ones(detect_time2.shape) * np.mean(tr_earthquake[0].data), 'kx')
-ax[1, 1].sharey(axi)
+    ax[1, 1].plot(waveform_time / second_per_day, tr_earthquake[0].data, color=ENZ_color[0])
+    ax[1, 1].plot(detect_time2 / second_per_day, np.ones(detect_time2.shape) * np.mean(tr_earthquake[0].data), 'kx')
+    ax[1, 1].sharey(axi)
 
-ax[2, 1].plot(waveform_time / second_per_day, tr_earthquake[1].data, color=ENZ_color[1])
-ax[2, 1].plot(detect_time2 / second_per_day, np.ones(detect_time2.shape) * np.mean(tr_earthquake[0].data), 'kx')
-ax[2, 1].sharey(axi)
+    ax[2, 1].plot(waveform_time / second_per_day, tr_earthquake[1].data, color=ENZ_color[1])
+    ax[2, 1].plot(detect_time2 / second_per_day, np.ones(detect_time2.shape) * np.mean(tr_earthquake[1].data), 'kx')
+    ax[2, 1].sharey(axi)
 
-ax[3, 1].plot(waveform_time / second_per_day, tr_earthquake[2].data, color=ENZ_color[2])
-ax[3, 1].plot(detect_time2 / second_per_day, np.ones(detect_time2.shape) * np.mean(tr_earthquake[0].data), 'kx')
-ax[3, 1].sharey(axi)
+    ax[3, 1].plot(waveform_time / second_per_day, tr_earthquake[2].data, color=ENZ_color[2])
+    ax[3, 1].plot(detect_time2 / second_per_day, np.ones(detect_time2.shape) * np.mean(tr_earthquake[2].data), 'kx')
+    ax[3, 1].sharey(axi)
 
-ax[3, 1].set_xlim(2.547, 2.548)
-ax[3, 1].set_ylim(-5, 5)
-
-ax[3, 1].set_xlim(8.3615, 8.3645)
-ax[3, 1].set_ylim(-0.2, 0.2)
-
-ax[3, 1].set_xlim(11.825, 11.84)
-ax[3, 1].set_ylim(-0.5, 0.5)
-
-ax[3, 1].set_xlim(0, 31)
-ax[3, 1].set_ylim(-1, 1)
-
-
-# Loop version, TODO: not show the entire waveform, only show part!
-trace_list = [tr_raw, tr_earthquake]
-cft_list = [[cft1_E, cft1_N, cft1_Z], [cft2_E, cft2_N, cft2_Z]]
-component_list = ['E', 'N', 'Z']
-name_list = ['Original waveform', 'Separated waveform']
+# This part is used to visually find the time window range for zoom in
 plt.close('all')
-fig, ax = plt.subplots(4, 2, sharex=True, figsize=(14, 12))
-axi = ax[1, 0]
+plot_long_waveform_for_visualization()
 
-for i_chan in range(3):
-    for i_tr, tr_waveform in enumerate(trace_list):
-        ax[0, i_tr].plot(waveform_time / second_per_day, cft_list[i_tr][i_chan], color=ENZ_color[i_chan])
-        ax[0, i_tr].plot(waveform_time[[0, -1]] / second_per_day, np.ones((2, 1)) * trigger_on, color='k')
-        ax[0, i_tr].plot(waveform_time[[0, -1]] / second_per_day, np.ones((2, 1)) * trigger_off, '--k')
-
-        ax[0, i_tr].set_title(name_list[i_tr])
-        ax[0, i_tr].set_ylabel('STA/LTA ratio')
-
-for i_tr, tr_waveform in enumerate(trace_list):
-    for i_chan in range(3):
-        ax[i_chan + 1, i_tr].plot(waveform_time / second_per_day, tr_waveform[i_chan].data, color=ENZ_color[i_chan])
-        ax[i_chan + 1, i_tr].plot(detect_time1 / second_per_day, \
-                                  np.ones(detect_time1.shape) * np.mean(tr_waveform[i_chan].data), 'kx')
-        ax[i_chan + 1, i_tr].sharey(axi)
-
-        ax[i_chan + 1, 0].set_ylabel(component_list[i_chan] + ' component')
-
-
-
-
-
-
-
+# plot the histogram for detected earthquakes
 plt.figure()
-
 plt.hist(detect_time2 / second_per_day, bins=31)
 plt.hist(detect_time1 / second_per_day, bins=31)
+
+# Loop version to plot better Zoom-in figure
+def plot_zoom_in_waveform(time_range):
+    waveform_time_in_day = waveform_time / second_per_day
+    detect_time_in_day1 = detect_time1 / second_per_day 
+    detect_time_in_day2 = detect_time2 / second_per_day
+
+    index_range = (waveform_time_in_day >= time_range[0]) & (waveform_time_in_day <= time_range[1])
+
+    # time in day showing in waveform
+    waveform_time_in_day = waveform_time_in_day[index_range]
+    detect_time_in_day1 = detect_time_in_day1[
+        (detect_time_in_day1 >= time_range[0]) & (detect_time_in_day1 <= time_range[1])]
+    detect_time_in_day2 = detect_time_in_day2[
+        (detect_time_in_day2 >= time_range[0]) & (detect_time_in_day2 <= time_range[1])]
+    detect_time_list = [detect_time_in_day1, detect_time_in_day2]
+
+    # change to datetime format
+    start_time = tr_earthquake[0].stats.starttime.timestamp
+    time_in_datetime = np.array([datetime.datetime.fromtimestamp(
+        waveform_time_point * second_per_day + start_time).strftime("%m-%d-%I:%M:%S")
+                                 for waveform_time_point in waveform_time_in_day])
+
+    # waveform data in the corresponding range
+    tr_raw_data = [tr_raw[j].data[index_range] for j in range(3)]
+    tr_earthquake_data = [tr_earthquake[j].data[index_range] for j in range(3)]
+
+    trace_list = [tr_raw_data, tr_earthquake_data]
+    cft_list = [[cft1_E[index_range], cft1_N[index_range], cft1_Z[index_range]],
+                [cft2_E[index_range], cft2_N[index_range], cft2_Z[index_range]]]
+
+    component_list = ['E', 'N', 'Z']
+    name_list = ['Original waveform', 'Separated waveform']
+    plt.close('all')
+    fig, ax = plt.subplots(4, 2, sharex=True, figsize=(14, 12))
+    axi = ax[1, 0]
+
+    for i_chan in range(3):
+        for i_tr, tr_waveform in enumerate(trace_list):
+            ax[0, i_tr].plot(waveform_time_in_day, cft_list[i_tr][i_chan],
+                             color=ENZ_color[i_chan], label=component_list[i_chan])
+
+            if i_chan == 2:
+                ax[0, i_tr].plot(waveform_time_in_day[[0, -1]], np.ones((2, 1)) * trigger_on,
+                                 color='k', label='Trigger ON')
+                ax[0, i_tr].plot(waveform_time_in_day[[0, -1]], np.ones((2, 1)) * trigger_off,
+                                 '--k', label='Trigger OFF')
+                ax[0, 1].sharey(ax[0, 0])
+
+            ax[0, i_tr].set_title(name_list[i_tr])
+            ax[0, i_tr].set_ylabel('STA/LTA ratio')
+
+    for i_tr, tr_waveform in enumerate(trace_list):
+        detect_time_in_day = detect_time_list[i_tr]
+        for i_chan in range(3):
+            ax[i_chan + 1, i_tr].plot(waveform_time_in_day, tr_waveform[i_chan], color=ENZ_color[i_chan])
+            ax[i_chan + 1, i_tr].plot(detect_time_in_day,
+                                      np.ones(detect_time_in_day.shape) * np.mean(tr_waveform[i_chan]), 'kx')
+            ax[i_chan + 1, i_tr].sharey(axi)
+
+            ax[i_chan + 1, 0].set_ylabel(component_list[i_chan] + ' component')
+
+            ax[i_chan + 1, i_tr].set_xticks(waveform_time_in_day[[0, int(len(waveform_time_in_day) / 2), -1]])
+            ax[i_chan + 1, i_tr].set_xticklabels(time_in_datetime[[0, int(len(waveform_time_in_day) / 2), -1]])
+
+    for i_tr in range(2):
+        ax[0, i_tr].legend()
+
+
+# output some zoom_in_figure
+output_dir = waveform_dir + '/STALTA'
+mkdir(output_dir)
+mkdir(output_dir + '/' + network_station)
+
+waveform_time = np.arange(tr_raw[0].stats.npts) * tr_raw[0].stats.delta
+
+time_range = [11.825, 11.84]
+time_range = [8.3615, 8.3645]
+time_range = [21.0675, 21.07]
+
+time_range_list = [[8.3615, 8.3645], [11.825, 11.84], [21.0675, 21.07]]
+for time_range in time_range_list:
+    plot_zoom_in_waveform(time_range)
+    file_name = network_station + '_t_' + str(time_range[0]) + '.pdf'
+    plt.savefig(output_dir + '/' + network_station + '/' + file_name)
+
+
+
+
+
