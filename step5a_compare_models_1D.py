1f71afd39e5cf4e8ae605cc1ba78705aec1fdc04
Jiuxun Yin <jiuxun_yin@g.harvard.edu>
2021-11-14 14:55:01 -0500

    Update the comparison between different bottlenecks.

--- step5a_compare_models_1D.py
+++ step5a_compare_models_1D.py
@@ -20,29 +20,29 @@ def signal_to_noise_ratio(signal, noise):
 
 # %% load dataset
 data_dir = './training_datasets'
-data_name = 'training_datasets_STEAD_waveform.hdf5'
+data_name = 'training_datasets_all.hdf5'
 # data_name = 'training_datasets_waveform.hdf5'
 # data_name = 'training_datasets_STEAD_plus_POHA.hdf5'
 
 # %% load dataset
 with h5py.File(data_dir + '/' + data_name, 'r') as f:
-    time = f['time'][:]
+    time = f['time_new'][:]
     X_train = f['X_train'][:]
     Y_train = f['Y_train'][:]
 
 # %% Specify the model directory and model name list first
-model_dataset_dir = "Model_and_datasets_1D_STEAD2"
+model_dataset_dir = "Model_and_datasets_1D_all"
 # model_dataset_dir = "Model_and_datasets_1D_synthetic"
 # model_dataset_dir = "Model_and_datasets_1D_STEAD_plus_POHA"
 # model_names = ["Autoencoder_Conv1D_None", "Autoencoder_Conv1D_Linear",
 #                "Autoencoder_Conv1D_LSTM", "Autoencoder_Conv1D_attention",
 #                "Autoencoder_Conv1D_attention_LSTM", "Autoencoder_Conv1D_Transformer"]
-model_names = ["Autoencoder_Conv2D_None", "Autoencoder_Conv2D_Linear",
-               "Autoencoder_Conv2D_LSTM", "Autoencoder_Conv2D_attention",
-               "Autoencoder_Conv2D_Transformer"]
+model_names = ["Branch_Encoder_Decoder_None", "Branch_Encoder_Decoder_Linear",
+               "Branch_Encoder_Decoder_LSTM", "Branch_Encoder_Decoder_attention",
+               "Branch_Encoder_Decoder_Transformer"]
 
 # make the output directory
-output_dir = model_dataset_dir + "/" + "all_model_comparison_2D"
+output_dir = model_dataset_dir + "/" + "all_model_comparison"
 mkdir(output_dir)
 
 model_mse_all = []  # list to store the mse for all models
@@ -81,9 +81,9 @@ for model_name in model_names:
         if len(y.data) != batch_size:
             break
         # forward pass: compute predicted outputs by passing inputs to the model
-        output = model(X)
+        output1, output2 = model(X)
         # calculate the loss
-        loss = loss_fn(output, y)
+        loss = loss_fn(output1, y) + loss_fn(output2, X - y)
         # update test loss
         test_loss += loss.item() * X.size(0)
 
@@ -99,17 +99,21 @@ for model_name in model_names:
         if len(y.data) != batch_size:
             break
         # forward pass: compute predicted outputs by passing inputs to the model
-        output = model(X)
+        output1, output2 = model(X)
 
         clean_signal = y.detach().numpy()
         clean_signal = np.reshape(clean_signal, (-1, clean_signal.shape[-1]))
         clean_signal = np.moveaxis(clean_signal, -1, 0)
 
-        noise = X.detach().numpy() - y.detach().numpy()
+        clean_noise = (X - y).detach().numpy()
+        clean_noise = np.reshape(clean_noise, (-1, clean_noise.shape[-1]))
+        clean_noise = np.moveaxis(clean_noise, -1, 0)
+
+        noise = output2.detach().numpy()
         noise = np.reshape(noise, (-1, noise.shape[-1]))
         noise = np.moveaxis(noise, -1, 0)
 
-        denoised_signal = output.detach().numpy()
+        denoised_signal = output1.detach().numpy()
         denoised_signal = np.reshape(denoised_signal, (-1, denoised_signal.shape[-1]))
         denoised_signal = np.moveaxis(denoised_signal, -1, 0)
 
@@ -119,6 +123,7 @@ for model_name in model_names:
 
         # calculate the mse between waveforms
         mse = explained_variance_score(clean_signal, denoised_signal, multioutput='raw_values')
+
         model_mse.append(mse)
 
     model_mse = np.array(model_mse).flatten()
@@ -144,15 +149,17 @@ import matplotlib
 matplotlib.rcParams.update({'font.size': 12})
 
 # %% Specify the model directory and model name list first
-model_dataset_dir = "Model_and_datasets_1D_STEAD2"
+model_dataset_dir = "Model_and_datasets_1D_all"
 # model_dataset_dir = "Model_and_datasets_1D_synthetic"
 # model_dataset_dir = "Model_and_datasets_1D_STEAD_plus_POHA"
-output_dir = model_dataset_dir + "/" + "all_model_comparison_2D"
+output_dir = model_dataset_dir + "/" + "all_model_comparison"
 
 with h5py.File(output_dir + '/all_model_comparison.hdf5', 'r') as f:
     model_names = f['model_names'][:]
     model_mse_all = f['model_mse_all'][:]
     model_snr_all = f['model_snr_all'][:]
+
+model_mse_all[model_mse_all < -2] = -2 # Force very low mse value
 
 plt.close('all')
 plt.figure(3)
@@ -200,35 +207,59 @@ plt.savefig(output_dir + '/histograms.png', dpi=200, bbox_inches='tight')
 
 # Extract the relation between SNR and EVS
 bin_size = 0.5
-snr_bin_edge = np.arange(-2, 1, bin_size)
+snr_bin_edge = np.arange(-2, 4, bin_size)
 snr_bin_center = snr_bin_edge + bin_size / 2
 mse_median_all = []
 mse_mean_all = []
 mse_std_all = []
+mse_error_bar_all = []
+
 for i in range(len(model_names)):
     mse_median = []
     mse_mean = []
     mse_std = []
     model_snr = model_snr_all[i] / 10
     model_mse = model_mse_all[i]
+
+    mse_error_positive = []
+    mse_error_negative = []
+
     for bin in snr_bin_edge:
+
         ii_bin = np.bitwise_and(model_snr <= (bin + bin_size), model_snr >= bin)
-        mse_median.append(np.median(model_mse[ii_bin]))
-        mse_mean.append(np.mean(model_mse[ii_bin]))
-        mse_std.append(np.std(model_mse[ii_bin]))
+
+        mse_current = model_mse[ii_bin]
+        # Median
+        mse_median_current = np.median(mse_current)
+        mse_median.append(mse_median_current)
+        # Mean
+        mse_mean_current = np.mean(mse_current)
+        mse_mean.append(mse_mean_current)
+        # STD (not used here)
+        mse_std.append(np.std(mse_current))
+
+        # # Use the mean values above and below the MEAN to inllustrate uncertainty
+        # mse_error_positive.append(np.mean(mse_current[mse_current > mse_mean_current]) - mse_mean_current)
+        # mse_error_negative.append(mse_mean_current - np.mean(mse_current[mse_current < mse_mean_current]))
+
+        # Use the mean values above and below the MEDIAN to inllustrate uncertainty
+        mse_error_positive.append(np.mean(mse_current[mse_current > mse_median_current]) - mse_median_current)
+        mse_error_negative.append(mse_median_current - np.mean(mse_current[mse_current < mse_median_current]))
+
 
     mse_median_all.append(np.array(mse_median))
-    mse_mean_all.append(np.array(mse_median))
+    mse_mean_all.append(np.array(mse_mean))
     mse_std_all.append(np.array(mse_std))
+    mse_error_bar_all.append(np.array([mse_error_negative, mse_error_positive]))
 
-plt.figure(2, figsize=(8, 4))
+plt.figure(2, figsize=(10, 5))
 for i in range(len(model_names)):
-    plt.plot(model_snr_all[i] / 10, model_mse_all[i], '.', color=line_colors[i], alpha=0.01)
-    plt.errorbar(snr_bin_center + i * 0.05 - 0.125, mse_mean_all[i], yerr=mse_std_all[i],
+    plt.plot(model_snr_all[i] / 10, model_mse_all[i], '.', color='gray', alpha=0.005) #line_colors[i]
+    plt.errorbar(snr_bin_center + i * 0.05 - 0.125, mse_median_all[i], yerr=mse_error_bar_all[i],
                  marker='s', color=line_colors[i], linewidth=2,
                  label=bottleneck_names[i], elinewidth=1.5, zorder=3)
-    plt.xlim(-2, 1)
-    plt.ylim(-0.3, 1.12)
+    plt.xlim(-2, 4)
+    plt.ylim(-2.1, 1.1)
 plt.legend(loc=4)
 plt.xlabel('log10(SNR)', fontsize=15)
 plt.ylabel('Explained Variance', fontsize=15)
@@ -240,11 +271,11 @@ plt.savefig(output_dir + '/SNR_vs_EV.png', dpi=200, bbox_inches='tight')
 ############################## Compare Conv1D and Conv2D models ######################################
 
 # %% Specify the model directory and model name list first
-model_dataset_dir = "Model_and_datasets_1D_STEAD2"
+model_dataset_dir = "Model_and_datasets_1D_all"
 # model_dataset_dir = "Model_and_datasets_1D_synthetic"
 # model_dataset_dir = "Model_and_datasets_1D_STEAD_plus_POHA"
-output_dir = model_dataset_dir + "/" + "all_model_comparison_1D"
-
+# output_dir = model_dataset_dir + "/" + "all_model_comparison_1D"
+output_dir = model_dataset_dir + "/" + "all_model_comparison_branches"
 
 with h5py.File(output_dir + '/all_model_comparison.hdf5', 'r') as f:
     model_names = f['model_names'][:]
@@ -348,3 +379,4 @@ plt.grid()
 
 # plt.savefig(output_dir + '/SNR_vs_EV.pdf')
 plt.savefig(output_dir + '/SNR_vs_EV.png', dpi=200, bbox_inches='tight')
+
