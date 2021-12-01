eb159ec200575f3561939577f44cbfdfa2c500c1
yinjiuxun <jiuxun_yin@g.harvard.edu>
2021-11-30 19:00:16 -0500

    Update with new models, and also check the EVS for both earthquake waveform and noise

--- step5a_compare_models_1D.py
+++ step5a_compare_models_1D.py
@@ -46,6 +46,7 @@ output_dir = model_dataset_dir + "/" + "all_model_comparison"
 mkdir(output_dir)
 
 model_mse_earthquake_all = []  # list to store the mse for all models
+model_mse_noise_all = []  # list to store the noise mse for all models
 model_snr_all = []  # list to store the snr for all models
 
 for model_name in model_names:
@@ -60,9 +61,9 @@ for model_name in model_names:
         rand_seed2 = f.attrs['rand_seed2']
 
     X_training, X_test, Y_training, Y_test = train_test_split(X_train, Y_train,
-                                            train_size=train_size, random_state=rand_seed1)
+                                                              train_size=train_size, random_state=rand_seed1)
     X_validate, X_test, Y_validate, Y_test = train_test_split(X_test, Y_test,
-                                            test_size=test_size, random_state=rand_seed2)
+                                                              test_size=test_size, random_state=rand_seed2)
 
     # first check the SNR distribution in each datasets
     SNR_training = signal_to_noise_ratio(Y_training, X_training - Y_training, axis=1) / 10
@@ -106,6 +107,7 @@ for model_name in model_names:
     # Calculate the MSE distribution for each model
     test_iter = DataLoader(test_data, batch_size=batch_size, shuffle=False)
     model_mse_earthquake = []
+    model_mse_noise = []
     model_snr = []
 
     for X, y in test_iter:
@@ -131,7 +133,7 @@ for model_name in model_names:
         denoised_signal = np.moveaxis(denoised_signal, -1, 0)
 
         # calculate the SNR
-        snr = signal_to_noise_ratio(clean_signal, noise)
+        snr = signal_to_noise_ratio(clean_signal, clean_noise)
         model_snr.append(snr)
 
         # calculate the mse between waveforms
@@ -139,11 +141,14 @@ for model_name in model_names:
         mse_noise = explained_variance_score(clean_noise, noise, multioutput='raw_values')
 
         model_mse_earthquake.append(mse)
+        model_mse_noise.append(mse_noise)
 
     model_mse_earthquake = np.array(model_mse_earthquake).flatten()
+    model_mse_noise = np.array(model_mse_noise).flatten()
     model_snr = np.array(model_snr).flatten()
 
     model_mse_earthquake_all.append(model_mse_earthquake)
+    model_mse_noise_all.append(model_mse_noise)
     model_snr_all.append(model_snr)
 
 # Save the mse and SNR of all models
@@ -152,6 +157,7 @@ model_comparison = output_dir + '/all_model_comparison.hdf5'
 with h5py.File(model_comparison, 'w') as f:
     f.create_dataset('model_names', data=model_names)
     f.create_dataset('model_mse_earthquake_all', data=model_mse_earthquake_all)
+    f.create_dataset('model_mse_noise_all', data=model_mse_noise_all)
     f.create_dataset('model_snr_all', data=model_snr_all)
 
 # Load the saved model comparison
@@ -159,6 +165,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 import h5py
 import matplotlib
+from scipy.stats import mode
 
 matplotlib.rcParams.update({'font.size': 12})
 
@@ -171,15 +178,14 @@ output_dir = model_dataset_dir + "/" + "all_model_comparison"
 with h5py.File(output_dir + '/all_model_comparison.hdf5', 'r') as f:
     model_names = f['model_names'][:]
     model_mse_earthquake_all = f['model_mse_earthquake_all'][:]
+    model_mse_noise_all = f['model_mse_noise_all'][:]
     model_snr_all = f['model_snr_all'][:]
 
-#model_mse_earthquake_all[model_mse_earthquake_all < -2] = -2 # Force very low mse value
+# model_mse_earthquake_all[model_mse_earthquake_all < -2] = -2 # Force very low mse value
 
 plt.close('all')
-plt.figure(3)
-fig, ax = plt.subplots(2, 2, sharex=True, sharey=True, squeeze=True)
+fig, ax = plt.subplots(2, 1, sharex=True, sharey=True, squeeze=True, figsize=(8, 8))
 ax = ax.flatten()
-i_waveforms = np.random.choice(len(model_mse_earthquake_all[0]), 10000)
 
 bottleneck_names = ["None", "Linear", "LSTM", "Attention", "Transformer"]
 line_colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22',
@@ -187,34 +193,28 @@ line_colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b',
 
 for i, model_name in enumerate(model_names):
     model_mse_earthquake = model_mse_earthquake_all[i]
+    model_mse_noise = model_mse_noise_all[i]
     model_snr = model_snr_all[i]
     bottleneck_name = bottleneck_names[i]
-    ii2 = np.bitwise_and(model_mse_earthquake <= 1, model_mse_earthquake >= -1)
-
-    hist, bin_edge = np.histogram(model_mse_earthquake[ii2], bins=20, range=(-1, 1))
-    hist = hist / len(model_mse_earthquake[ii2])
+    ii1 = np.bitwise_and(model_mse_earthquake <= 1, model_mse_earthquake >= -1)
+    hist, bin_edge = np.histogram(model_mse_earthquake[ii1], bins=20, range=(-1, 1))
+    hist = hist / len(model_mse_earthquake[ii1])
     bin_center = bin_edge[0:-1] + (bin_edge[1] - bin_edge[0]) / 2
-    plt.figure(1, figsize=(8, 4))
-    plt.plot(bin_center, hist, '-o', color=line_colors[i], label=bottleneck_name)
-    #
-    # plt.figure(2)
-    # ii1 = np.bitwise_and(model_snr <= 20, model_snr >= -40)
-    # ii2 = np.bitwise_and(model_mse_earthquake <= 1, model_mse_earthquake >= 0)
-    # sns.violinplot(x=model_name, y=model_mse_earthquake[ii2], alpha=0.5)
-    #
-    #
-    # plt.figure(3)
-    # ax[i].plot(model_snr, model_mse_earthquake, '.', label=model_name, alpha=0.1)
-    # ax[i].set_ylim(0, 1)
-    # ax[i].set_xlim(-40, 20)
-    #
-    #
+    ax[0].plot(bin_center, hist, '-o', color=line_colors[i], label=bottleneck_name + ' earthquake')
+    ax[0].grid()
 
-plt.figure(1)
-plt.xlabel('Explained variance score')
-plt.ylabel('Probability Density')
-plt.legend()
-plt.grid()
+    ii2 = np.bitwise_and(model_mse_noise <= 1, model_mse_noise >= -1)
+    hist, bin_edge = np.histogram(model_mse_noise[ii2], bins=20, range=(-1, 1))
+    hist = hist / len(model_mse_noise[ii2])
+    bin_center = bin_edge[0:-1] + (bin_edge[1] - bin_edge[0]) / 2
+    ax[1].plot(bin_center, hist, '-d', color=line_colors[i], label=bottleneck_name + ' noise')
+    ax[1].grid()
+
+ax[1].set_xlabel('Explained variance score')
+ax[0].set_ylabel('Probability Density')
+ax[1].set_ylabel('Probability Density')
+ax[0].legend()
+ax[1].legend()
 
 plt.savefig(output_dir + '/histograms.pdf')
 plt.savefig(output_dir + '/histograms.png', dpi=200, bbox_inches='tight')
@@ -222,54 +222,91 @@ plt.savefig(output_dir + '/histograms.png', dpi=200, bbox_inches='tight')
 # Extract the relation between SNR and EVS
 bin_size = 0.5
 snr_bin_edge = np.arange(-2, 4, bin_size)
-snr_bin_center = snr_bin_edge + bin_size / 2
-mse_median_all = []
-mse_mean_all = []
-mse_std_all = []
-mse_error_bar_all = []
 
-for i in range(len(model_names)):
-    mse_median = []
-    mse_mean = []
-    mse_std = []
-    model_snr = model_snr_all[i] / 10
-    model_mse_earthquake = model_mse_earthquake_all[i]
+def continous_mode(data, bins=500, range=(-2, 1)):
+    hist, bin_edge = np.histogram(data, bins=bins, range=range)
+    bin_center = bin_edge[0:-1] + (bin_edge[1] - bin_edge[0]) / 2
+    #plt.plot(bin_center, hist)
+    return bin_center[np.argmax(hist)]
 
-    mse_error_positive = []
-    mse_error_negative = []
+def extract_snr_vs_evs(model_snr_all, model_mse_all, snr_bin_edge):
+    snr_bin_center = snr_bin_edge + bin_size / 2
+    mse_median_all = []
+    mse_mean_all = []
+    mse_mode_all = []
+    mse_std_all = []
+    mse_error_bar_all = []
 
-    for bin in snr_bin_edge:
+    for i in range(len(model_names)):
+        mse_median = []
+        mse_mean = []
+        mse_mode = []
+        mse_std = []
+        model_snr = model_snr_all[i] / 10
+        model_mse = model_mse_all[i]
 
-        ii_bin = np.bitwise_and(model_snr <= (bin + bin_size), model_snr >= bin)
+        mse_error_positive = []
+        mse_error_negative = []
 
-        mse_current = model_mse_earthquake[ii_bin]
-        # Median
-        mse_median_current = np.median(mse_current)
-        mse_median.append(mse_median_current)
-        # Mean
-        mse_mean_current = np.mean(mse_current)
-        mse_mean.append(mse_mean_current)
-        # STD (not used here)
-        mse_std.append(np.std(mse_current))
+        for bin in snr_bin_edge:
+            ii_bin = np.bitwise_and(model_snr <= (bin + bin_size), model_snr >= bin)
 
-        # # Use the mean values above and below the MEAN to inllustrate uncertainty
-        # mse_error_positive.append(np.mean(mse_current[mse_current > mse_mean_current]) - mse_mean_current)
-        # mse_error_negative.append(mse_mean_current - np.mean(mse_current[mse_current < mse_mean_current]))
-
-        # Use the mean values above and below the MEDIAN to inllustrate uncertainty
-        mse_error_positive.append(np.median(mse_current[mse_current > mse_median_current]) - mse_median_current)
-        mse_error_negative.append(mse_median_current - np.median(mse_current[mse_current < mse_median_current]))
+            mse_current = model_mse[ii_bin]
+            # Median
+            mse_median_current = np.median(mse_current)
+            mse_median.append(mse_median_current)
+            # Mean
+            mse_mean_current = np.mean(mse_current)
+            mse_mean.append(mse_mean_current)
+            # STD (not used here)
+            mse_std.append(np.std(mse_current))
+            # Statistic mode
+            mse_mode_current = continous_mode(mse_current)
+            mse_mode.append(mse_mode_current)
 
 
-    mse_median_all.append(np.array(mse_median))
-    mse_mean_all.append(np.array(mse_mean))
-    mse_std_all.append(np.array(mse_std))
-    mse_error_bar_all.append(np.array([mse_error_negative, mse_error_positive]))
+            # # Use the mean values above and below the MEAN to inllustrate uncertainty
+            # mse_error_positive.append(np.mean(mse_current[mse_current > mse_mean_current]) - mse_mean_current)
+            # mse_error_negative.append(mse_mean_current - np.mean(mse_current[mse_current < mse_mean_current]))
 
+            # Use the mean values above and below the MEDIAN to inllustrate uncertainty
+            mse_error_positive.append(np.median(mse_current[mse_current > mse_median_current]) - mse_median_current)
+            mse_error_negative.append(mse_median_current - np.median(mse_current[mse_current < mse_median_current]))
+
+        mse_median_all.append(np.array(mse_median))
+        mse_mean_all.append(np.array(mse_mean))
+        mse_mode_all.append(np.array(mse_mode))
+        mse_std_all.append(np.array(mse_std))
+        mse_error_bar_all.append(np.array([mse_error_negative, mse_error_positive]))
+
+        mse_center_all = mse_mode_all
+
+    return snr_bin_center, mse_center_all, mse_error_bar_all
+
+
+# show earthquake waveforms evs
+snr_bin_center, mse_center_all, mse_error_bar_all = extract_snr_vs_evs(model_snr_all,
+                                                                       model_mse_earthquake_all, snr_bin_edge)
 plt.figure(2, figsize=(10, 5))
 for i in range(len(model_names)):
-    plt.plot(model_snr_all[i] / 10, model_mse_all[i], '.', color='gray', alpha=0.005) #line_colors[i]
-    plt.errorbar(snr_bin_center + i * 0.05 - 0.125, mse_median_all[i], yerr=mse_error_bar_all[i],
+    plt.plot(model_snr_all[i] / 10, model_mse_earthquake_all[i], '.', color='gray', alpha=0.005)  # line_colors[i]
+    plt.errorbar(snr_bin_center + i * 0.05 - 0.125, mse_center_all[i], yerr=mse_error_bar_all[i],
+                 marker='s', color=line_colors[i], linewidth=2,
+                 label=bottleneck_names[i], elinewidth=1.5, zorder=3)
+    plt.xlim(-2, 4)
+    plt.ylim(-2.1, 1.1)
+plt.legend(loc=4)
+plt.xlabel('log10(SNR)', fontsize=15)
+plt.ylabel('Explained Variance', fontsize=15)
+plt.grid()
+
+# show ambient noise waveforms evs
+snr_bin_center, mse_center_all, mse_error_bar_all = extract_snr_vs_evs(model_snr_all,
+                                                                       model_mse_noise_all, snr_bin_edge)
+plt.figure(2, figsize=(10, 5))
+for i in range(len(model_names)):
+    plt.plot(model_snr_all[i] / 10, model_mse_noise_all[i], '.', color='gray', alpha=0.005)  # line_colors[i]
+    plt.errorbar(snr_bin_center + i * 0.05 - 0.125, mse_center_all[i], yerr=mse_error_bar_all[i],
                  marker='s', color=line_colors[i], linewidth=2,
                  label=bottleneck_names[i], elinewidth=1.5, zorder=3)
     plt.xlim(-2, 4)
@@ -319,7 +356,6 @@ plt.figure(3)
 fig, ax = plt.subplots(2, 2, sharex=True, sharey=True, squeeze=True)
 ax = ax.flatten()
 i_waveforms = np.random.choice(len(model_mse_all[0]), 10000)
-
 
 for i, model_name in enumerate(range(len(index_model_to_show))):
     model_mse = model_mse_all[index_model_to_show[i]]
@@ -393,4 +429,3 @@ plt.grid()
 
 # plt.savefig(output_dir + '/SNR_vs_EV.pdf')
 plt.savefig(output_dir + '/SNR_vs_EV.png', dpi=200, bbox_inches='tight')
-
