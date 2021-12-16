38ca19ce6bcb6e1725bf5767e6efca3ba4866b0b
yinjiuxun <jiuxun_yin@g.harvard.edu>
2021-12-15 23:15:56 -0500

    Polished the plotting of CCFs

--- auto_correlation_noise.py
+++ auto_correlation_noise.py
@@ -12,7 +12,7 @@ from obspy.taup import TauPyModel
 from obspy.geodetics import locations2degrees
 
 import matplotlib
-matplotlib.rcParams.update({'font.size': 18})
+matplotlib.rcParams.update({'font.size': 15})
 from utilities import mkdir
 
 # %%
@@ -119,17 +119,17 @@ def running_mean_spectrum(X, N):
     return fftconvolve(abs(X), np.ones((X.shape[0], N)) / N, mode='same', axes=1)
 
 
-def calculate_xcorf(waveform1, waveform2):
+def calculate_xcorf(waveform1, waveform2, running_mean_samples=32):
     """Calculate cross-correlation functions for single station,
     waveform1 and waveform2 are (nun_windows, num_time_points, )"""
     spectra_data_1 = fft(waveform1, axis=1)
     spectra_data_1[0] = 0
     # spectra_data_1 = spectra_data_1 / abs(spectra_data_1)
-    spectra_data_1 = spectra_data_1 / (running_mean_spectrum(spectra_data_1, 32) + 1e-8)
+    spectra_data_1 = spectra_data_1 / (running_mean_spectrum(spectra_data_1, running_mean_samples) + 1e-8)
     spectra_data_2 = fft(waveform2, axis=1)
     spectra_data_2[0] = 0
     # spectra_data_2 = spectra_data_2 / abs(spectra_data_2)
-    spectra_data_2 = spectra_data_2 / (running_mean_spectrum(spectra_data_2, 32) + 1e-8)
+    spectra_data_2 = spectra_data_2 / (running_mean_spectrum(spectra_data_2, running_mean_samples) + 1e-8)
     xcorf = ifft(spectra_data_1 * np.conjugate(spectra_data_2), axis=1)
     return xcorf
 
@@ -145,10 +145,10 @@ channel_xcor_list = []
 for i in range(3):
     for j in range(3):
         print(channel_list[i] + ' - ' + channel_list[j])
-        channel_xcor_list.append(channel_list[i] + ' - ' + channel_list[j])
+        channel_xcor_list.append(channel_list[i] + '-' + channel_list[j])
         k += 1
-        xcorf_function1[:, :, k] = np.real(calculate_xcorf(data_test1[:, :, i], data_test1[:, :, j]))
-        xcorf_function2[:, :, k] = np.real(calculate_xcorf(data_test2[:, :, i], data_test2[:, :, j]))
+        xcorf_function1[:, :, k] = np.real(calculate_xcorf(data_test1[:, :, i], data_test1[:, :, j], 32))
+        xcorf_function2[:, :, k] = np.real(calculate_xcorf(data_test2[:, :, i], data_test2[:, :, j], 32))
 
 # with h5py.File(waveform_output_dir + '/' + bottleneck_name + '_xcorr_functions.hdf5', 'w') as f:
 #     f.create_dataset("original_xcor", data=xcorf_function1, compression="gzip")
@@ -185,9 +185,16 @@ def average_xcorr_functions(xcorf_funciton, average_hours, time_pts_xcorf, dt, b
 
 
 # Calculate the correlation coef with the global average
-def plot_correlation_coefficent(average_acf1, average_acf2, xcorf_day_time, figure_name, title=None):
+def plot_correlation_coefficent(average_acf1, average_acf2, xcorf_day_time, xcorf_time=None,
+                                figure_name=None, title=None):
+    plt.close('all')
     line_label = ['original waveform', 'separated noise']
     fig, ax = plt.subplots(9, 1, sharex=True, sharey=True, figsize=(4, 14))
+
+    if xcorf_time is not None:
+        index_time = (xcorf_time_lag >= xcorf_time[0]) & (xcorf_time_lag <= xcorf_time[1])
+        average_acf1 = average_acf1[:, index_time, :]
+        average_acf2 = average_acf2[:, index_time, :]
 
     for index_channel in range(9):
 
@@ -206,16 +213,111 @@ def plot_correlation_coefficent(average_acf1, average_acf2, xcorf_day_time, figu
                           'x', color='k', linewidth=4)
 
             ax[index_channel].set_ylabel(channel_xcor_list[index_channel] + ' coef', fontsize=14)
-            ax[index_channel].set_ylim(-1.3, 1.4)
-            ax[index_channel].set_yticks([-1, 1])
+            ax[index_channel].set_ylim(0, 1.4)
+            #ax[index_channel].set_yticks([-1, 1])
             if index_channel == 0:
                 if title is not None:
                     ax[index_channel].set_title(title)
 
             if index_channel == 8:
                 ax[index_channel].set_xlabel('Time (day)', fontsize=14)
-                ax[index_channel].legend(fontsize=14)
-    plt.savefig(figure_name, bbox_inches='tight', dpi=150)
+                ax[index_channel].legend(fontsize=14, loc=(0.1, -1.3))
+    if figure_name is not None:
+        plt.savefig(figure_name, bbox_inches='tight', dpi=150)
+
+
+def compare_ccfs_plot(scale_factor, time_extent, t_ballistic_coda):
+    plt.close('all')
+    #scale_factor = 6
+    #extent = [0, time_pts_xcorf * dt, 0, 31]
+
+    fig, ax = plt.subplots(9, 9, figsize=(12, 14),
+                           gridspec_kw={'width_ratios': [3,3,1,3,3,1,3,3,1],
+                                        'height_ratios': [1,4,0.5,1,4,0.5,1,4,0.5]})
+    ax_ref=ax[1, 0]
+    #fig.suptitle('(a) Raw waveform')
+    k = -1
+    for i in range(3):
+        for j in range(3):
+            k += 1
+            ## Raw data
+            # show stacked CCF
+            ref_ccf = np.mean(average_acf1[:, :, k], axis=0)
+            ref_ccf = ref_ccf/np.max(abs(ref_ccf))
+            ax[i*3, j*3].plot(xcorf_time_lag, ref_ccf, '-k')
+            ax[i*3, j*3].axis('off')
+            ax[i*3, j*3].annotate(f'({str(chr(k+97))}) ' + channel_xcor_list[k], xy=(-0.4, 1.2), xycoords=ax[i*3, j*3].transAxes)
+            ax[i*3, j*3].set_title('Raw', fontsize=12)
+            ax[i*3, j*3].sharex(ax_ref)
+
+
+            # show CCFs of each channel
+            norm_color = cm.colors.Normalize(vmax=abs(average_acf1[:, :, k]).max() / scale_factor,
+                                                 vmin=-abs(average_acf1[:, :, k]).max() / scale_factor)
+
+            ax[i*3+1, j*3].imshow(average_acf1[:, :, k], norm=norm_color, cmap='RdBu', aspect='auto',
+                            extent=[0, time_pts_xcorf * dt, 0, 31], origin='lower')
+
+            ax[i*3+1, j*3].plot(np.ones(event_arrival_S.shape) * (time_extent[0] * 0.05 + time_extent[1] * 0.95),
+                          event_arrival_S/24/3600, marker='.', color='g', linestyle='None')
+
+            ax[i*3+1, j*3].axvline(x=t_ballistic_coda, linestyle='-', color='k', linewidth=1)
+            ax[i*3+1, j*3].sharey(ax_ref)
+            ax[i*3+1, j*3].sharex(ax_ref)
+
+
+            ax[i*3+1, j*3].axes.xaxis.set_visible(False)
+            ax[i*3+1, j*3].axes.yaxis.set_visible(False)
+            ax[i*3+1, j*3].set_xlim(time_extent[0], time_extent[1])
+            ax[i*3+1, j*3].set_xticks(np.floor(np.linspace(int(time_extent[0]),int(time_extent[1]),3,endpoint=False)))
+
+            if j == 0:
+                ax[i*3+1, j*3].set_ylabel('Days')
+                ax[i*3+1, j*3].axes.yaxis.set_visible(True)
+            if i == 2:
+                ax[i*3+1, j*3].set_xlabel('Time (s)')
+                ax[i*3+1, j*3].axes.xaxis.set_visible(True)
+
+
+            ## Decomposed noise
+            # show stacked CCF
+            ref_ccf = np.mean(average_acf2[:, :, k], axis=0)
+            ref_ccf = ref_ccf / np.max(abs(ref_ccf))
+            ax[i*3, j*3+1].plot(xcorf_time_lag, ref_ccf, '-k')
+            ax[i*3, j*3+1].axis('off')
+            ax[i*3, j*3+1].set_title('Decomposed', fontsize=12)
+            ax[i*3, j*3+1].sharex(ax_ref)
+
+            # show CCFs of each channel
+            norm_color = cm.colors.Normalize(vmax=abs(average_acf2[:, :, k]).max() / scale_factor,
+                                                 vmin=-abs(average_acf2[:, :, k]).max() / scale_factor)
+
+            ax[i*3+1, j*3+1].imshow(average_acf2[:, :, k], cmap='RdBu', norm=norm_color, aspect='auto',
+                            extent=[0, time_pts_xcorf * dt, 0, 31], origin='lower')
+
+            ax[i*3+1, j*3+1].plot(np.ones(event_arrival_S.shape) * (time_extent[0] * 0.05 + time_extent[1] * 0.95),
+                          event_arrival_S / 24 / 3600, marker='.', color='g', linestyle='None')
+
+
+            ax[i*3+1, j*3+1].axvline(x=t_ballistic_coda, linestyle='-', color='k', linewidth=1)
+            ax[i*3+1, j*3+1].sharey(ax_ref)
+            ax[i*3+1, j*3+1].sharex(ax_ref)
+
+            ax[i*3+1, j*3+1].axes.xaxis.set_visible(False)
+            ax[i*3+1, j*3+1].axes.yaxis.set_visible(False)
+            ax[i*3+1, j*3+1].set_xlim(time_extent[0], time_extent[1])
+            ax[i*3+1, j*3+1].set_xticks(np.floor(np.linspace(int(time_extent[0]),int(time_extent[1]),3,endpoint=False)))
+
+            if i == 2:
+                ax[i*3+1, j*3+1].set_xlabel('Time (s)')
+                ax[i*3+1, j*3+1].axes.xaxis.set_visible(True)
+
+            ax[i*3, j * 3 + 2].axis('off')
+            ax[i*3+1, j * 3 + 2].axis('off')
+            ax[i * 3 + 2, j * 3].axis('off')
+            ax[i * 3 + 2, j * 3 + 1].axis('off')
+            ax[i*3+2, j * 3 + 2].axis('off')
+
 
 
 dt = waveform_time[1] - waveform_time[0]
@@ -229,6 +331,63 @@ bandpass_filter = None
 _, _, average_acf1 = average_xcorr_functions(xcorf_function1, average_hours, time_pts_xcorf, dt, bandpass_filter)
 xcorf_time_lag, xcorf_day_time, average_acf2 = \
     average_xcorr_functions(xcorf_function2, average_hours, time_pts_xcorf, dt, bandpass_filter)
+
+compare_ccfs_plot(scale_factor=10, time_extent=[10, 20])
+
+
+# Results with bandpassing filtering
+frequency_bands = [[0.1, 1], [1, 2], [2, 4]]
+order_letter = ['(a)', '(b)', '(c)']
+scale_factor = [10, 10, 6]
+time_extent_list = [[0, 20], [0, 12], [0, 6]]
+t_sep = [20, 3.5, 2]
+
+for ii, frequency_band in enumerate(frequency_bands):
+    bandpass_filter = np.array(frequency_band)  # [0.1, 1] [1, 2][2, 4.5]
+    file_name_str = '_' + str(bandpass_filter[0]) + '_' + str(bandpass_filter[1]) + 'Hz'
+
+    _, _, average_acf1 = average_xcorr_functions(xcorf_function1, average_hours, time_pts_xcorf, dt, bandpass_filter)
+    xcorf_time_lag, xcorf_day_time, average_acf2 = \
+        average_xcorr_functions(xcorf_function2, average_hours, time_pts_xcorf, dt, bandpass_filter)
+    compare_ccfs_plot(scale_factor=scale_factor[ii], time_extent=time_extent_list[ii], t_ballistic_coda=t_sep[ii])
+    plt.savefig(waveform_output_dir + '/ccf_comparison' + file_name_str + '.png', dpi=150)
+
+    figure_name = waveform_output_dir + '/cc_coda' + file_name_str + '.pdf'
+    plot_correlation_coefficent(average_acf1, average_acf2, xcorf_day_time, xcorf_time=time_extent_list[ii],
+                                figure_name=figure_name, title=order_letter[ii])
+
+    figure_name = waveform_output_dir + '/cc_ballistic' + file_name_str + '.pdf'
+    plot_correlation_coefficent(average_acf1, average_acf2, xcorf_day_time, xcorf_time=[0, time_extent_list[ii][0]],
+                                figure_name=figure_name, title=order_letter[ii])
+
+
+
+####################
+bandpass_filter = np.array([0.1, 1])  # [0.1, 1] [1, 2][2, 4.5]
+_, _, average_acf1 = average_xcorr_functions(xcorf_function1, average_hours, time_pts_xcorf, dt, bandpass_filter)
+xcorf_time_lag, xcorf_day_time, average_acf2 = \
+    average_xcorr_functions(xcorf_function2, average_hours, time_pts_xcorf, dt, bandpass_filter)
+compare_ccfs_plot(scale_factor=10, time_extent=[0, 20])
+plot_correlation_coefficent(average_acf1, average_acf2, xcorf_day_time, xcorf_time=[0, 20])
+
+bandpass_filter = np.array([1, 2])  # [0.1, 1] [1, 2][2, 4.5]
+_, _, average_acf1 = average_xcorr_functions(xcorf_function1, average_hours, time_pts_xcorf, dt, bandpass_filter)
+xcorf_time_lag, xcorf_day_time, average_acf2 = \
+    average_xcorr_functions(xcorf_function2, average_hours, time_pts_xcorf, dt, bandpass_filter)
+compare_ccfs_plot(scale_factor=30, time_extent=[3, 12])
+
+plot_correlation_coefficent(average_acf1, average_acf2, xcorf_day_time, xcorf_time=[0, 3])
+
+bandpass_filter = np.array([2, 4.5])  # [0.1, 1] [1, 2][2, 4.5]
+_, _, average_acf1 = average_xcorr_functions(xcorf_function1, average_hours, time_pts_xcorf, dt, bandpass_filter)
+xcorf_time_lag, xcorf_day_time, average_acf2 = \
+    average_xcorr_functions(xcorf_function2, average_hours, time_pts_xcorf, dt, bandpass_filter)
+compare_ccfs_plot(scale_factor=10, time_extent=[2, 6])
+
+plot_correlation_coefficent(average_acf1, average_acf2, xcorf_day_time, xcorf_time=[2, 6])
+
+
+
 
 plt.close('all')
 scale_factor = 6
@@ -409,3 +568,4 @@ for i in range(3):
             ax[i, j].set_ylabel('Days')
         if i == 2:
             ax[i, j].set_xlabel('Time (s)')
+
